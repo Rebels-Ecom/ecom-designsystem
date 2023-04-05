72cb6a2cdb75027fa4efc3ec979029374b50b4ff
Thomas Kjellberg <thomas.kjellberg@rebelandbird.com>
2023-04-05 17:41:57 +0200

    Add base url to the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import copy from 'rollup-plugin-copy'
 /* https://vitejs.dev/config/ */
 
 export default defineConfig({
+  base: '/ecom-designsystem/',
   server: { https: true },
   plugins: [
     react(),
