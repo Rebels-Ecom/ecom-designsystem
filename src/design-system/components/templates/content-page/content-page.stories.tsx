b885855b0646a7fe3de1037a351889fdf562f4d1
JelenaCumbo <jelena.cumbo@rebelandbird.com>
2023-04-11 09:07:03 +0200

    Fix issue with blog cards not showing on content-page.

--- src/design-system/components/templates/content-page/content-page.stories.tsx
+++ src/design-system/components/templates/content-page/content-page.stories.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { ContentPage } from './content-page'
 import { HeroCarouselStory } from '../../organisms/hero-carousel/hero-carousel.stories'
-import { BlogCardListStory_4_Columns, BlogCardListStory_3_Columns } from '../../organisms/blog-card-list/blog-card-list.stories'
+import { BlogCardListStory_VarförSpendrups, BlogCardListStory_Samarbete } from '../../organisms/blog-card-list/blog-card-list.stories'
 import { OfferCardListStory } from '../../organisms/offer-card-list/offer-card-list.stories'
 import { BlogCardStoryFullWidth } from '../../molecules/blog-card/blog-card.stories'
-import { TeaserStoryLeft, TeaserStoryRight } from '../../molecules/teaser/teaser.stories'
+import { TeaserStoryLeft1, TeaserStoryRight } from '../../molecules/teaser/teaser.stories'
 import { HeaderStory } from '../../organisms/header/header.stories'
 import { FooterStory } from '../../organisms/footer/footer.stories'
 import { FeaturedProductsCarouselStory } from '../../organisms/featured-products-carousel/featured-products-carousel.stories'
@@ -28,14 +28,14 @@ export const ContentPageTemplateStory = {
   args: {
     header: HeaderStory.args,
     hero: HeroCarouselStory.args,
-    blogList_4_Col: BlogCardListStory_4_Columns.args,
+    blogList_4_Col: BlogCardListStory_VarförSpendrups.args,
     featuredProducts: FeaturedProductsCarouselStory.args,
     offerCardsList: OfferCardListStory.args,
     blogFullwidth: BlogCardStoryFullWidth.args,
     customerTeaser: CustomerTeaserStory.args,
     teaserRight: TeaserStoryRight.args,
-    blogList_3_Col: BlogCardListStory_3_Columns.args,
-    teaserLeft: TeaserStoryLeft.args,
+    blogList_3_Col: BlogCardListStory_Samarbete.args,
+    teaserLeft: TeaserStoryLeft1.args,
     footer: FooterStory.args,
   },
 }
