82861509345bc4a1619f3958f515e66506ba7485
JelenaCumbo <jelena.cumbo@rebelandbird.com>
2023-04-12 08:45:07 +0200

    Add video component.

--- src/design-system/components/molecules/hero/hero.tsx
+++ src/design-system/components/molecules/hero/hero.tsx
@@ -2,13 +2,14 @@ import cx from 'classnames'
 import styles from './hero.module.css'
 import { IPicture, TPictureFetchPriority, TPictureLoading, Picture } from '../../atoms/picture/picture'
 import { ILinkButton, LinkButton } from '../../atoms/link-button/link-button'
-import { ILink } from '../../../../types/links'
+import { IVideo, Video } from '../../atoms/video/video'
 
 export type alignContent = 'left' | 'right' | 'center'
 export type heroTheme = 'light' | 'dark'
 
 export interface IHero {
   image: IPicture
+  video?: IVideo
   contentImage?: IPicture
   topHeading?: string
   heading: string
@@ -18,6 +19,7 @@ export interface IHero {
   heroTheme?: heroTheme
   fetchPriority?: TPictureFetchPriority
   loading?: TPictureLoading
+  isVideo?: boolean
 }
 
 function getAlignContent(alignContent: string = 'left'): string {
@@ -31,10 +33,15 @@ function getHeroTheme(heroTheme: string = 'light'): string {
   return styles.lightTheme
 }
 
-const Hero = ({ topHeading, heading, preamble, image, contentImage, alignContent, link, heroTheme='light', fetchPriority, loading }: IHero) => {
+const Hero = ({ topHeading, heading, preamble, image, video, contentImage, alignContent, link, heroTheme='light', fetchPriority, loading, isVideo }: IHero) => {
   return (
     <section className={styles.hero}>
-      <Picture {...image} fetchPriority={fetchPriority} loading={loading} classNamePicture={styles.picture} classNameImg={cx(styles.image)} pictureWithOpacity={heroTheme}/>
+      {(isVideo && video)
+        ?
+        <Video {...video} videoWithOpacity={heroTheme}></Video>
+        :
+        <Picture {...image} fetchPriority={fetchPriority} loading={loading} classNamePicture={styles.picture} classNameImg={cx(styles.image)} pictureWithOpacity={heroTheme}/>
+      }
       <div className={styles.outerWrapper}>
         <div className={styles.innerWrapper}>
           <div className={cx(styles.content, getAlignContent(alignContent), getHeroTheme(heroTheme))}>
