668abaf90ab553d3fb3774bff6e271da217fa1b8
JelenaCumbo <jelena.cumbo@rebelandbird.com>
2023-07-19 17:36:21 +0200

    Add modifications to modal.

--- src/design-system/components/molecules/modal/modal.stories.tsx
+++ src/design-system/components/molecules/modal/modal.stories.tsx
@@ -4,49 +4,49 @@ import { Modal } from './modal'
 import { CreateListForm } from '../../organisms/create-list-form/create-list-form'
 import { CreateListFormStory } from '../../organisms/create-list-form/create-list-form.stories'
 
-
 const meta: Meta<typeof Modal> = {
   title: 'Design System/Molecules/Modal',
-  component: Modal
-};
+  component: Modal,
+}
 
-export default meta;
-type Story = StoryObj<typeof Modal>;
+export default meta
+type Story = StoryObj<typeof Modal>
 
 const ModalStoryTemplate: Story = {
-  render: ({...args}) => {
-    const[isOpen, setIsOpen] = useState<boolean>(false)
+  render: ({ ...args }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
     return (
-    <div>
-        <button onClick={()=>setIsOpen(true)}>Open Modal</button>
-        <div id="portal"></div>
-        <Modal {...args} open={isOpen} onClose={()=>setIsOpen(false)} >
-            <CreateListForm {...CreateListFormStory.args} onCancel={()=>setIsOpen(false)}></CreateListForm>
+      <div>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+
+        <Modal {...args} open={isOpen} onClose={() => setIsOpen(false)}>
+          <CreateListForm {...CreateListFormStory.args} onCancel={() => setIsOpen(false)}></CreateListForm>
         </Modal>
-    </div>)
-  }
-};
+      </div>
+    )
+  },
+}
 
 export const ModalStory = {
   ...ModalStoryTemplate,
   args: {
-    children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>
-  }
+    children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>,
+  },
 }
 
 export const ModalStory_Dismissable = {
-    ...ModalStoryTemplate,
-    args: {
-      children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>,
-      dismissable: true
-    }
+  ...ModalStoryTemplate,
+  args: {
+    children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>,
+    dismissable: true,
+  },
 }
 
 export const ModalStory_LightBackdrop = {
-    ...ModalStoryTemplate,
-    args: {
-      children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>,
-      dismissable: true,
-      backdropType: 'light'
-    }
+  ...ModalStoryTemplate,
+  args: {
+    children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>,
+    dismissable: true,
+    backdropType: 'light',
+  },
 }
