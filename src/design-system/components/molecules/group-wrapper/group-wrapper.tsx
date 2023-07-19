0ec4459fb346cd1d528ffb4840e50b8caa609abb
JelenaCumbo <jelena.cumbo@rebelandbird.com>
2023-07-19 14:57:38 +0200

    Add modification for group-wrapper.

--- src/design-system/components/molecules/group-wrapper/group-wrapper.tsx
+++ src/design-system/components/molecules/group-wrapper/group-wrapper.tsx
@@ -3,88 +3,92 @@ import styles from './group-wrapper.module.css'
 import cx from 'classnames'
 
 export type TGroupDirection = 'default' | 'row' | 'column'
-export type TGroupPosition = 'left' | 'right' | 'center' |'apart'
+export type TGroupPosition = 'left' | 'right' | 'center' | 'apart'
 export type TSpacing = number | 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-export type TAlign = 'left' | 'right' | 'center' |'baseline'
+export type TAlign = 'left' | 'right' | 'center' | 'baseline'
 
 export interface IGroupWrapper {
-    children: React.ReactNode
-    direction?: TGroupDirection
-    position?: TGroupPosition
-    spacing?: TSpacing
-    noWrap?: boolean
-    align?: TAlign
+  children: React.ReactNode
+  direction?: TGroupDirection
+  position?: TGroupPosition
+  spacing?: TSpacing
+  fullWidth?: boolean
+  noWrap?: boolean
+  align?: TAlign
 }
 
-function GroupWrapper({ children,  direction='default', position='left', spacing='md', noWrap= true, align='left' } : IGroupWrapper){
-
-    function getGroupElementsPosition(position: TGroupPosition){
-        switch (position) {
-            case 'left':
-              return 'leftPosition'
-            case 'right':
-              return 'rightPosition'
-            case 'center':
-                return 'centerPosition'
-            case 'apart':
-            default:
-              return 'apartPosition'
-        }
+function GroupWrapper({ children, direction = 'default', position = 'left', spacing = 'md', noWrap = true, align = 'left', fullWidth = true }: IGroupWrapper) {
+  function getGroupElementsPosition(position: TGroupPosition) {
+    switch (position) {
+      case 'left':
+        return 'leftPosition'
+      case 'right':
+        return 'rightPosition'
+      case 'center':
+        return 'centerPosition'
+      case 'apart':
+      default:
+        return 'apartPosition'
     }
-
-    function getGroupElementsAlignment(alignment: TAlign){
-        switch (alignment) {
-            case 'left':
-              return 'leftAlignment'
-            case 'right':
-              return 'rightAlignment'
-            case 'center':
-                return 'centerAlignment'
-            case 'baseline':
-            default:
-              return 'baselineAlignment'
-        }
-    }
-
-    function getGroupElementsSpacing(spacing: TSpacing){
-        switch (spacing) {
-            case 'xs':
-              return 'spacing-xs'
-            case 'sm':
-              return 'spacing-sm'
-            case 'md':
-                return 'spacing-md'
-            case 'lg':
-                return 'spacing-lg'
-            case 'xl':
-            default:
-              return 'spacing-xl'
-        }
-    }
-
-    function getGroupElementsDirection(direction: TGroupDirection){
-      switch (direction) {
-          case 'column':
-            return 'direction-column'
-          case 'row':
-            return 'direction-row'
-          case 'default':
-          default:
-            return 'direction-default'
-      }
   }
 
-    return(
-        <div className={cx(
-            styles.groupWrapper, 
-            styles[getGroupElementsDirection(direction)],
-            styles[getGroupElementsPosition(position)], 
-            styles[getGroupElementsAlignment(align)], 
-            styles[getGroupElementsSpacing(spacing)], 
-            noWrap && styles.noWrap)} >
-                {children}
-        </div>
-    )
+  function getGroupElementsAlignment(alignment: TAlign) {
+    switch (alignment) {
+      case 'left':
+        return 'leftAlignment'
+      case 'right':
+        return 'rightAlignment'
+      case 'center':
+        return 'centerAlignment'
+      case 'baseline':
+      default:
+        return 'baselineAlignment'
+    }
+  }
+
+  function getGroupElementsSpacing(spacing: TSpacing) {
+    switch (spacing) {
+      case 'xs':
+        return 'spacing-xs'
+      case 'sm':
+        return 'spacing-sm'
+      case 'md':
+        return 'spacing-md'
+      case 'lg':
+        return 'spacing-lg'
+      case 'xl':
+      default:
+        return 'spacing-xl'
+    }
+  }
+
+  function getGroupElementsDirection(direction: TGroupDirection) {
+    switch (direction) {
+      case 'column':
+        return 'direction-column'
+      case 'row':
+        return 'direction-row'
+      case 'default':
+      default:
+        return 'direction-default'
+    }
+  }
+
+  return (
+    <div
+      className={cx(
+        styles.groupWrapper,
+        styles[getGroupElementsDirection(direction)],
+        styles[getGroupElementsPosition(position)],
+        styles[getGroupElementsAlignment(align)],
+        styles[getGroupElementsSpacing(spacing)],
+        fullWidth ? styles.fullWidth : '',
+        noWrap && styles.noWrap
+      )}
+    >
+      {children}
+    </div>
+  )
 }
 
-export { GroupWrapper } 
+export { GroupWrapper }
