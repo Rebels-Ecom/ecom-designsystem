f7d20a7f45664267f70597801cd5fa169e309d3f
Robert Pettersson <robert.pettersson@rebelandbird.com>
2023-09-27 15:54:09 +0200

    only allow positive numbers in product quantity input, add bottom border to desktop navigation

--- src/design-system/components/molecules/product-quantity-input/product-quantity-input.tsx
+++ src/design-system/components/molecules/product-quantity-input/product-quantity-input.tsx
@@ -29,7 +29,27 @@ const ProductQuantityInput = ({
 
     return (
         <div className = {`${styles.inputQuantityWrapper} ${className ? className : ''}`}>
-            <InputText id={quantityInputId} defaultValue={quantity} placeholder={'0'} onChange={handleOnChange} disabled={disabled} type={'number'} className={styles.quantityInputField} min='0' />
+            <InputText
+                id={quantityInputId}
+                defaultValue={quantity}
+                placeholder='0'
+                onChange={handleOnChange}
+                disabled={disabled}
+                type='number'
+                className={styles.quantityInputField}
+                min='0'
+                other={{
+                    onKeyDown: (e: React.KeyboardEvent) => {
+                        const illegalChars = ['-', '+', '.', ',']
+                        if (illegalChars.find(p => p === e.key)) {
+                            e.preventDefault()
+                        }
+                    },
+                    onPaste: (e: React.ClipboardEvent) => {
+                        e.preventDefault();
+                    }
+                }}
+            />
             <div>
                 <p className={`${styles.textQuantity} bodyS`}>{`x ${salesUnit} (${itemNumberPerSalesUnit} stick)`}</p>
                 <p className={`${styles.textPrice} bodyS fontBold`}>{`Pris: ${totalPrice} kr`}</p>
