1773c8d3023368a4129b3a051eef55a86b4688ab
Robert Pettersson <robert.pettersson@rebelandbird.com>
2023-09-27 17:39:08 +0200

    improve responsiveness

--- src/design-system/components/layouts/breakpoints/index.ts
+++ src/design-system/components/layouts/breakpoints/index.ts
@@ -1,3 +1,3 @@
 export type Breakpoints = { xs: number; sm: number; md: number; lg: number; xl: number }
 export type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-export const breakpoints: Breakpoints = { xs: 0, sm: 576, md: 768, lg: 992, xl: 1200 }
+export const breakpoints: Breakpoints = { xs: 0, sm: 576, md: 768, lg: 1024, xl: 1440 }
