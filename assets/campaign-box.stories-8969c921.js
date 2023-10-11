4bf9f8c6b3f60cfe23965748b323c4f10e277c7d
bobby-rebel <bobby-rebel@users.noreply.github.com>
2023-10-11 07:47:23 +0000

    deploy: 8777cd28c5c72b71e2df1a3e59c9e4bc20bda1cf

---
+++ assets/campaign-box.stories-8969c921.js
@@ -0,0 +1,11 @@
+import{C as e}from"./campaign-box-2dff404f.js";import{a as i,F as s}from"./jsx-runtime-76c5c2e2.js";import"./index-74f03c09.js";import"./motion-354a1bac.js";import"./index-6f814c40.js";import"./_commonjsHelpers-042e6b4d.js";const u={title:"Design System/Atoms/CampaignBox",component:e},p={render:t=>i(s,{children:i(e,{...t})})},o={...p,args:{title:"Campaign Title",description:"Campaign Description",subDescription:"Campaign SubDescription",color:"#9A576F",onClick:()=>console.log("clicked")}};var a,n,r;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  ...CampaignBoxStoryTemplate,
+  args: {
+    title: 'Campaign Title',
+    description: 'Campaign Description',
+    subDescription: 'Campaign SubDescription',
+    color: '#9A576F',
+    onClick: () => console.log('clicked')
+  }
+}`,...(r=(n=o.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};const x=["CampaignBoxStory"];export{o as CampaignBoxStory,x as __namedExportsOrder,u as default};
+//# sourceMappingURL=campaign-box.stories-8969c921.js.map
