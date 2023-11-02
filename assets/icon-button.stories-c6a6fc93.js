4ff4d27ad09ed9dfda731d38ffa3f507f5aed9a8
JelenaCumbo <JelenaCumbo@users.noreply.github.com>
2023-11-02 12:41:46 +0000

    deploy: f12664a608d115ce945f3f5f47a1f0d2d6e02979

---
+++ assets/icon-button.stories-c6a6fc93.js
@@ -0,0 +1,72 @@
+import{I as E}from"./icon-button-e9add618.js";import{a as o}from"./chunk-OPEUWD42-0dc0813d.js";import{a as L}from"./jsx-runtime-76c5c2e2.js";import"./index-74f03c09.js";import"./ui-link-1bd4bf5e.js";import"./icon-af819d0c.js";import"./motion-354a1bac.js";import"./index-6f814c40.js";import"./_commonjsHelpers-042e6b4d.js";const F={title:"Design System/Atoms/IconButton",component:E},n={render:U=>L(E,{...U})},t={...n,args:{icon:"icon-map-pin",size:"small",noPadding:!1,isTransparent:!1,noBorder:!1,notification:2,type:"button",onClick:o("clicked")}},r={...n,args:{icon:"icon-map-pin",size:"small",noPadding:!1,isTransparent:!1,noBorder:!1,notification:102,type:"button",onClick:o("clicked")}},e={...n,args:{type:"link",icon:"icon-map-pin",size:"large",linkUrl:"/start"}},a={...n,args:{type:"link",icon:"icon-chevrons-right",size:"large",isExternal:!0,linkUrl:"/?path=/docs/design-system-templates-productpage--docs",linkComponent:"a"}},s={...n,args:{type:"button",icon:"icon-x-circle",size:"large",isTransparent:!0,onClick:o("clicked")}},c={...n,args:{type:"button",icon:"icon-shopping-cart",size:"large",isTransparent:!0,onClick:o("clicked")}},i={...n,args:{type:"button",icon:"icon-shopping-cart",size:"large",isTransparent:!0,children:L("span",{children:"(7)"}),onClick:o("clicked")}};var p,l,u;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...IconButtonStoryTemplate,
+  args: ({
+    icon: 'icon-map-pin',
+    size: 'small',
+    noPadding: false,
+    isTransparent: false,
+    noBorder: false,
+    notification: 2,
+    type: 'button',
+    onClick: action('clicked')
+  } as TIconButton)
+}`,...(u=(l=t.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var m,d,g;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  ...IconButtonStoryTemplate,
+  args: ({
+    icon: 'icon-map-pin',
+    size: 'small',
+    noPadding: false,
+    isTransparent: false,
+    noBorder: false,
+    notification: 102,
+    type: 'button',
+    onClick: action('clicked')
+  } as TIconButton)
+}`,...(g=(d=r.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var y,B,k;e.parameters={...e.parameters,docs:{...(y=e.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  ...IconButtonStoryTemplate,
+  args: ({
+    type: 'link',
+    icon: 'icon-map-pin',
+    size: 'large',
+    linkUrl: '/start'
+  } as TIconButton)
+}`,...(k=(B=e.parameters)==null?void 0:B.docs)==null?void 0:k.source}}};var I,S,T;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  ...IconButtonStoryTemplate,
+  args: ({
+    type: 'link',
+    icon: 'icon-chevrons-right',
+    size: 'large',
+    isExternal: true,
+    linkUrl: '/?path=/docs/design-system-templates-productpage--docs',
+    linkComponent: 'a'
+  } as TIconButton)
+}`,...(T=(S=a.parameters)==null?void 0:S.docs)==null?void 0:T.source}}};var f,C,h;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  ...IconButtonStoryTemplate,
+  args: ({
+    type: 'button',
+    icon: 'icon-x-circle',
+    size: 'large',
+    isTransparent: true,
+    onClick: action('clicked')
+  } as TIconButton)
+}`,...(h=(C=s.parameters)==null?void 0:C.docs)==null?void 0:h.source}}};var z,P,b;c.parameters={...c.parameters,docs:{...(z=c.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  ...IconButtonStoryTemplate,
+  args: ({
+    type: 'button',
+    icon: 'icon-shopping-cart',
+    size: 'large',
+    isTransparent: true,
+    onClick: action('clicked')
+  } as TIconButton)
+}`,...(b=(P=c.parameters)==null?void 0:P.docs)==null?void 0:b.source}}};var _,x,v;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  ...IconButtonStoryTemplate,
+  args: ({
+    type: 'button',
+    icon: 'icon-shopping-cart',
+    size: 'large',
+    isTransparent: true,
+    children: <span>(7)</span>,
+    onClick: action('clicked')
+  } as TIconButton)
+}`,...(v=(x=i.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};const H=["IconButtonStorySmallButton","IconButtonStoryOver100Products","IconButtonStoryLargeLink","IconButtonStory_GoToProduct","IconButtonStory_RemoveProduct","IconButtonStory_CartEmpty","IconButtonStory_CartWithProducts"];export{e as IconButtonStoryLargeLink,r as IconButtonStoryOver100Products,t as IconButtonStorySmallButton,c as IconButtonStory_CartEmpty,i as IconButtonStory_CartWithProducts,a as IconButtonStory_GoToProduct,s as IconButtonStory_RemoveProduct,H as __namedExportsOrder,F as default};
+//# sourceMappingURL=icon-button.stories-c6a6fc93.js.map
