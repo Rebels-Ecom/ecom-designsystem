2f6c56b0582f007d67e1abf2aaef9f2c3733e492
Robert Pettersson <robert.pettersson@rebelandbird.com>
2023-11-06 14:43:25 +0100

    support mobile video url

--- src/design-system/components/atoms/video/video.tsx
+++ src/design-system/components/atoms/video/video.tsx
@@ -1,14 +1,23 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import styles from './video.module.css'
 import cx from 'classnames'
 
+export interface IVideoSource {
+  srcset?: string
+  type?: string
+  media?: string
+  sizes?: string
+}
+
 export interface IVideo {
-  videoUrl: string
-  videoWithOpacity?: string
-  className?: string
+  videoUrl: string;
+  mobileUrl?: string;
+  videoWithOpacity?: string;
+  className?: string;
 };
 
-const Video = ({videoUrl, videoWithOpacity, className} : IVideo) => {
+const Video = ({videoUrl, videoWithOpacity, className, mobileUrl} : IVideo) => {
+  const [source, setSource] = useState(videoUrl);
     const videoRef = useRef<HTMLVideoElement>(null)
 
     useEffect(() => {
@@ -18,12 +27,19 @@ const Video = ({videoUrl, videoWithOpacity, className} : IVideo) => {
           videoRef.current.muted = true
           videoRef.current.loop = true
         }
+
+        if (typeof window === 'undefined') return;
+
+        if (window.innerWidth <= 767 && mobileUrl) {
+          setSource(mobileUrl);
+        }
+
       }, [videoRef])
     
     return (
         <>
             <video ref={videoRef} playsInline className={cx(styles.video, className && className)}>
-                <source src={videoUrl} type="video/mp4" />
+              <source src={source} type="video/mp4" />
             </video>
             {videoWithOpacity && <div className={cx(styles.opacityLayer, videoWithOpacity==='light' ? styles.withLightBackground : styles.withDarkBackground)}/>}
         </>
