81a73f4a70a8837d1932dd4277e03fe2321f20f6
JelenaCumbo <jelena.cumbo@rebelandbird.com>
2023-11-07 18:12:28 +0100

    Add padding and border to OrderConfirmationDetails.

--- src/design-system/components/molecules/order-confirmation-details/order-confirmation-details.tsx
+++ src/design-system/components/molecules/order-confirmation-details/order-confirmation-details.tsx
@@ -2,29 +2,37 @@ import React from 'react'
 import styles from './order-confirmation-details.module.css'
 import cx from 'classnames'
 
+export type TPaddingSize = 'sm' | 'md' | 'lg'
 export interface IOrderConfirmationItem {
-    label: string
-    value: string
-    isTotal?: boolean
+  label: string
+  value: string
+  isTotal?: boolean
 }
 
 export interface IOrderConfirmationDetails {
-    detailItems: IOrderConfirmationItem[],
-    label?: string,
-    withBackground?: boolean
+  detailItems: IOrderConfirmationItem[]
+  label?: string
+  withBackground?: boolean
+  withBorder?: boolean
+  padding?: TPaddingSize
 }
 
-function OrderConfirmationDetails({detailItems, label, withBackground} : IOrderConfirmationDetails) {
+function OrderConfirmationDetails({ detailItems, label, withBackground, withBorder, padding = 'md' }: IOrderConfirmationDetails) {
   return (
-    <div className={cx('body', styles.detailItemWrapper, withBackground && styles.withBackground)}>
-          {label && <p className={styles.detailItem}><b>{label}</b></p>}
-          {
-            detailItems.map((item: IOrderConfirmationItem)=>
-                <p className={styles.detailItem}>
-                    <span>{item.label}</span>
-                    <span className={item.isTotal ? styles.totalAmount : ''}>{item.value}</span>
-                </p>)
-          }
+    <div
+      className={cx('body', styles.detailItemWrapper, withBackground && styles.withBackground, withBorder && styles.withBorder, styles[`padding-${padding}`])}
+    >
+      {label && (
+        <p className={styles.detailItem}>
+          <b>{label}</b>
+        </p>
+      )}
+      {detailItems.map((item: IOrderConfirmationItem, index: number) => (
+        <p className={styles.detailItem} key={`${item.label}-${index}`}>
+          <span>{item.label}</span>
+          <span className={item.isTotal ? styles.totalAmount : ''}>{item.value}</span>
+        </p>
+      ))}
     </div>
   )
 }
