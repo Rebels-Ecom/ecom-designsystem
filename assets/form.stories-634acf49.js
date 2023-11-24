645f8ce2f91e63f08625b67c9d2794ee370ea907
bobby-rebel <bobby-rebel@users.noreply.github.com>
2023-11-24 10:08:48 +0000

    deploy: c305233c95c569c18a278523b26e68d660668a4a

---
+++ assets/form.stories-634acf49.js
@@ -0,0 +1,71 @@
+import{a as d}from"./jsx-runtime-76c5c2e2.js";import{F as s}from"./form-a9f1b383.js";import"./index-6f814c40.js";import"./_commonjsHelpers-042e6b4d.js";import"./ui-link-da2491c1.js";import"./index-74f03c09.js";import"./icon-17f6a5b2.js";import"./motion-354a1bac.js";import"./heading-1b9cc643.js";import"./text-288eb4bf.js";import"./flex-container-c88ed1e9.js";import"./expandable-wrapper-911222ad.js";import"./button-689f1284.js";import"./button.module-3bdf7fd3.js";import"./loader-ce89f5e5.js";const x={title:"Design System/Atoms/Form",component:s},m={render:({...p})=>d(s,{...p})},e={...m,args:{formTitle:"Form Title",loading:!1,onSubmit:()=>console.log("submit"),fields:[{label:"Name",type:"text",fieldType:"input",name:"name",originalValue:"",placeholder:"Namn Namnsson",subtitle:"subtitle",required:!0,error:"This field is required"},{label:"Email",type:"email",fieldType:"input",name:"email",originalValue:"",subtitle:"subtitle",placeholder:"email@email.com",required:!0,pattern:"email",error:"Email is required, e.g. mail@mail.com"}],actions:[{children:"Save",surface:"primary",type:"submit"}],alignActions:"center"}},r={...m,args:{formTitle:"Form Title",loading:!1,onSubmit:()=>console.log("submit"),fields:[{label:"Nytt lösenord",type:"password",fieldType:"input",name:"new-password",originalValue:"",placeholder:"*",required:!0,error:"Du måste ange ett lösenord",betaField:"confirm-password"},{label:"Upprepa lösenord",type:"password",fieldType:"input",name:"confirm-password",originalValue:"",placeholder:"*",required:!0,error:"Lösenord matchar inte",alphaField:"new-password"}],actions:[{children:"Save",surface:"primary",type:"submit"}],alignActions:"center"}};var n,a,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...FormStoryTemplate,
+  args: ({
+    formTitle: 'Form Title',
+    loading: false,
+    onSubmit: () => console.log('submit'),
+    fields: [{
+      label: 'Name',
+      type: 'text',
+      fieldType: 'input',
+      name: 'name',
+      originalValue: '',
+      placeholder: 'Namn Namnsson',
+      subtitle: 'subtitle',
+      required: true,
+      error: 'This field is required'
+    }, {
+      label: 'Email',
+      type: 'email',
+      fieldType: 'input',
+      name: 'email',
+      originalValue: '',
+      subtitle: 'subtitle',
+      placeholder: 'email@email.com',
+      required: true,
+      pattern: 'email',
+      error: 'Email is required, e.g. mail@mail.com'
+    }],
+    actions: [{
+      children: 'Save',
+      surface: 'primary',
+      type: 'submit'
+    }],
+    alignActions: 'center'
+  } as IFormTemplateProps)
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var o,t,l;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...FormStoryTemplate,
+  args: ({
+    formTitle: 'Form Title',
+    loading: false,
+    onSubmit: () => console.log('submit'),
+    fields: [{
+      label: 'Nytt lösenord',
+      type: 'password',
+      fieldType: 'input',
+      name: 'new-password',
+      originalValue: '',
+      placeholder: '*',
+      required: true,
+      error: 'Du måste ange ett lösenord',
+      betaField: 'confirm-password'
+    }, {
+      label: 'Upprepa lösenord',
+      type: 'password',
+      fieldType: 'input',
+      name: 'confirm-password',
+      originalValue: '',
+      placeholder: '*',
+      required: true,
+      error: 'Lösenord matchar inte',
+      alphaField: 'new-password'
+    }],
+    actions: [{
+      children: 'Save',
+      surface: 'primary',
+      type: 'submit'
+    }],
+    alignActions: 'center'
+  } as IFormTemplateProps)
+}`,...(l=(t=r.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};const A=["Standard_Form","Compare_Two_Fields"];export{r as Compare_Two_Fields,e as Standard_Form,A as __namedExportsOrder,x as default};
+//# sourceMappingURL=form.stories-634acf49.js.map
