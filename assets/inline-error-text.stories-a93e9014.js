bfb19687f96bd21ba7809639281293a2d6af49f9
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-01-04 10:25:12 +0000

    deploy: 1b509faadce0b407c400cce37347ffb20c4e291c

---
+++ assets/inline-error-text.stories-a93e9014.js
@@ -0,0 +1,10 @@
+import{I as s}from"./inline-error-text-34a06e17.js";import{j as e}from"./jsx-runtime-a3a6c0b8.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";import"./icon-5fefd509.js";const c={title:"Design System/Atoms/Messages",component:s},r={render:({...i})=>e("div",{style:{margin:"0 0 1rem 0",maxWidth:"616px"},children:e(s,{children:"Fält är obligatorisk"})})};var t,n,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }) => <div style={{
+    margin: '0 0 1rem 0',
+    maxWidth: '616px'
+  }}>
+      <InlineErrorText>Fält är obligatorisk</InlineErrorText>
+    </div>
+}`,...(o=(n=r.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const x=["InlineErrorTextStory"];export{r as InlineErrorTextStory,x as __namedExportsOrder,c as default};
---
+++ assets/start-page-template.stories-a41ab30c.js
@@ -0,0 +1,55 @@
+import{H as m,a as H}from"./hero-carousel.stories-f9e764bd.js";import{T as g,F as t,a as n,B as o}from"./blog-card-list.stories-a477d888.js";import{O as u,a as E}from"./offer-card-list.stories-816ba385.js";import{T as l,a as s,b as d}from"./teaser.stories-a894f995.js";import{Standard_Header as _}from"./header.stories-cb741853.js";import{F as S,a as O}from"./footer.stories-368e4996.js";import{F as R,a as C}from"./featured-products-carousel.stories-9a57ee45.js";import{C as f,a as U}from"./customer-teaser.stories-9c49a201.js";import{B as L,a as x}from"./blog-card.stories-dc93c71d.js";import{a as c,F as j,j as e}from"./jsx-runtime-a3a6c0b8.js";import{C as A}from"./content-wrapper-c985da24.js";import"./hero-56751d68.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";import"./picture-78f09330.js";import"./link-button-1db7193c.js";import"./ui-link-a18343fc.js";import"./icon-5fefd509.js";import"./video-4611e7a5.js";import"./carousel-e9caa029.js";import"./hero.stories-5e56d235.js";import"./picture.stories-b9fdbc7d.js";import"./pang-f66faff3.js";import"./video.stories-f53502fc.js";import"./below-e3c1f628.js";import"./flex-container-145d4933.js";import"./motion-54668070.js";import"./offer-card.stories-c963712f.js";import"./tag-fac2d6a3.js";import"./tag.stories-8b101b34.js";import"./search-nav-bar.stories-1ffc9e02.js";import"./search-e8630c02.js";import"./input-text-aa9063aa.js";import"./form-helper-ce65ae5e.js";import"./button-8a1c536b.js";import"./button.module-d4e6cb42.js";import"./loader-72deb8c4.js";import"./useOnClickOutside-338168d6.js";import"./useCloseOnEscape-c2f46cc9.js";import"./Spendrups_logo_horizontal-038b757f.js";import"./Spendrups_logo_vertical-4f973801.js";import"./cart-sidebar-a3081d80.js";import"./ui-date-picker.stories-75e89b6f.js";import"./isNativeReflectConstruct-81b4d0cb.js";import"./index-79d56f34.js";import"./admin-search.stories-264ff975.js";import"./cart-product-list-c0ae9926.js";import"./cart-product-list.stories-2b9ed390.js";import"./cart-product.stories-4d33e466.js";import"./product-quantity-input-289618f1.js";import"./picture-helper-1d1954b7.js";import"./icon-button-2f0ea5c0.js";import"./format-helper-6b4b1091.js";import"./icon-button.stories-9df69175.js";import"./chunk-WFFRPTHA-a68c42c5.js";import"./preview-errors-dde4324f.js";import"./index-356e4a49.js";import"./mobile-navigation.stories-41a94d53.js";import"./index-4058f88b.js";import"./desktop-navigation.stories-b13ed6b9.js";import"./user-info-summary.stories-eae747ce.js";import"./dropdown-list.stories-53c8dc04.js";import"./top-nav-bar-06cd7d88.js";import"./delivery-info-bar-7d5f50d5.js";import"./logotype-5f577bc9.js";import"./expandable-wrapper-5e4804ab.js";import"./drawer-sidebar-8bbd569e.js";import"./group-wrapper-8f1ce8c7.js";import"./heading-a9520268.js";import"./form-group-953293a9.js";import"./inline-helper-text-1f598236.js";import"./inline-error-text-34a06e17.js";import"./toggle-switch-6a44e951.js";import"./newsletter-e81b7866.js";import"./footer-top-bar.stories-ea3aef96.js";import"./social-media-link-08763191.js";import"./product-carousel-26a842be.js";import"./product-card-49b24209.js";import"./defaultFallbackImage-b43581a6.js";import"./product-card-horizontal-cd3656ae.js";import"./tags-list-5108b032.js";import"./placeholder-0f82f781.js";import"./product-card-vertical-ed64d0da.js";import"./product-variant-list-390ee459.js";import"./product-variant-2a30c4d0.js";import"./radio-button-611b19a6.js";import"./product-card-list.stories-d2eb99ae.js";import"./product-card.stories-a68aa6ca.js";import"./dummy-product-2b5b59bc.js";import"./featured-products.stories-54dc1a5a.js";import"./signup-teaser.stories-1e6a041e.js";import"./ui-link.stories-7a85f29a.js";const be={title:"Design System/Templates/StartPageTemplate",parameters:{controls:{exclude:/.*/g,hideNoControlsWarning:!0}}},h={render:({...r})=>c(j,{children:[r.header&&r.header(),e(H,{...r.hero}),c(A,{children:[e(o,{...r.blogList_Why_Spendrups}),e(C,{...r.featuredProducts}),e(E,{...r.offerCardsList}),e(o,{...r.blogList_Spendrups_Suppliers}),e(x,{...r.blogFullwidth}),e(U,{...r.customerTeaser}),e(d,{...r.teaserLeft1}),e(o,{...r.blogList_Spendrups_Brewing}),e(d,{...r.teaserRight}),e(d,{...r.teaserLeft2}),e(o,{...r.blogList_Spendrups_Equipment}),e(C,{...r.featuredProducts})]}),e(O,{...r.footer})]})},a={...h,args:{header:_.render,hero:m.args,blogList_Why_Spendrups:g.args,featuredProducts:R.args,offerCardsList:u.args,blogList_Spendrups_Suppliers:t.args,blogFullwidth:L.args,customerTeaser:f.args,teaserRight:l.args,blogList_Spendrups_Brewing:n.args,teaserLeft1:s.args,teaserLeft2:s.args,blogList_Spendrups_Equipment:t.args,footer:S.args}},p={...h,args:{header:_.render,hero:m.args,blogList_Why_Spendrups:g.args,featuredProducts:R.args,offerCardsList:u.args,blogList_Spendrups_Suppliers:t.args,blogFullwidth:L.args,customerTeaser:f.args,teaserRight:l.args,blogList_Spendrups_Brewing:n.args,teaserLeft1:s.args,teaserLeft2:s.args,blogList_Spendrups_Equipment:t.args,footer:S.args}},i={...h,args:{header:_.render,hero:m.args,blogList_Why_Spendrups:t.args,featuredProducts:g.args,offerCardsList:u.args,blogList_Spendrups_Suppliers:t.args,blogFullwidth:L.args,customerTeaser:f.args,teaserRight:l.args,blogList_Spendrups_Brewing:n.args,teaserLeft1:s.args,teaserLeft2:s.args,blogList_Spendrups_Equipment:t.args,footer:S.args}};var F,b,T;a.parameters={...a.parameters,docs:{...(F=a.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  ...StartPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    hero: HeroCarouselStory.args,
+    blogList_Why_Spendrups: Three_Cards.args,
+    featuredProducts: FeaturedProductsCarouselStory.args,
+    offerCardsList: OfferCardListStory.args,
+    blogList_Spendrups_Suppliers: Four_Cards.args,
+    blogFullwidth: BlogCardStoryFullWidth.args,
+    customerTeaser: CustomerTeaserStory.args,
+    teaserRight: Teaser_Round.args,
+    blogList_Spendrups_Brewing: Full_Width.args,
+    teaserLeft1: Teaser_Square.args,
+    teaserLeft2: Teaser_Square.args,
+    blogList_Spendrups_Equipment: Four_Cards.args,
+    footer: FooterStory.args
+  }
+}`,...(T=(b=a.parameters)==null?void 0:b.docs)==null?void 0:T.source}}};var y,P,W;p.parameters={...p.parameters,docs:{...(y=p.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  ...StartPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    hero: HeroCarouselStory.args,
+    blogList_Why_Spendrups: Three_Cards.args,
+    featuredProducts: FeaturedProductsCarouselStory.args,
+    offerCardsList: OfferCardListStory.args,
+    blogList_Spendrups_Suppliers: Four_Cards.args,
+    blogFullwidth: BlogCardStoryFullWidth.args,
+    customerTeaser: CustomerTeaserStory.args,
+    teaserRight: Teaser_Round.args,
+    blogList_Spendrups_Brewing: Full_Width.args,
+    teaserLeft1: Teaser_Square.args,
+    teaserLeft2: Teaser_Square.args,
+    blogList_Spendrups_Equipment: Four_Cards.args,
+    footer: FooterStory.args
+  }
+}`,...(W=(P=p.parameters)==null?void 0:P.docs)==null?void 0:W.source}}};var B,q,w;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  ...StartPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    hero: HeroCarouselStory.args,
+    blogList_Why_Spendrups: Four_Cards.args,
+    featuredProducts: Three_Cards.args,
+    offerCardsList: OfferCardListStory.args,
+    blogList_Spendrups_Suppliers: Four_Cards.args,
+    blogFullwidth: BlogCardStoryFullWidth.args,
+    customerTeaser: CustomerTeaserStory.args,
+    teaserRight: Teaser_Round.args,
+    blogList_Spendrups_Brewing: Full_Width.args,
+    teaserLeft1: Teaser_Square.args,
+    teaserLeft2: Teaser_Square.args,
+    blogList_Spendrups_Equipment: Four_Cards.args,
+    footer: FooterStory.args
+  }
+}`,...(w=(q=i.parameters)==null?void 0:q.docs)==null?void 0:w.source}}};const Te=["StartPage_Guest_User","StartPage_Logged_In_User","StartPage_Admin_User"];export{i as StartPage_Admin_User,a as StartPage_Guest_User,p as StartPage_Logged_In_User,Te as __namedExportsOrder,be as default};
