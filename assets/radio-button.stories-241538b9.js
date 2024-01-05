aa324dcfea8de773b7ef8ae182195aefb3fb3de1
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-01-05 13:35:23 +0000

    deploy: 133236f92d165bec2d6deed6f17543b48702c123

---
+++ assets/radio-button.stories-241538b9.js
@@ -0,0 +1,23 @@
+import{R as g}from"./radio-button-d43d9462.js";import{j as a}from"./jsx-runtime-a3a6c0b8.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";const b={title:"Design System/Atoms/Inputs/Radio Button",component:g},e={render:({...S})=>a("div",{style:{margin:"0 0 2rem 0",maxWidth:"616px"},children:a(g,{...S})})},r={...e,args:{id:"input_1",value:"input_1",name:"group_1",checked:!0,disabled:!1,onChange:()=>{}}},o={...e,args:{...r.args,isErroneous:!0}},t={...e,args:{...r.args,disabled:!0}};var n,s,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...RadioButtonStoryTemplate,
+  args: {
+    id: 'input_1',
+    value: 'input_1',
+    name: 'group_1',
+    checked: true,
+    disabled: false,
+    onChange: () => {}
+  }
+}`,...(d=(s=r.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};var i,u,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  ...RadioButtonStoryTemplate,
+  args: {
+    ...RadioButtonStory.args,
+    isErroneous: true
+  }
+}`,...(m=(u=o.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var p,c,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...RadioButtonStoryTemplate,
+  args: {
+    ...RadioButtonStory.args,
+    disabled: true
+  }
+}`,...(l=(c=t.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const h=["RadioButtonStory","RadioButtonStoryError","RadioButtonStoryDisabled"];export{r as RadioButtonStory,t as RadioButtonStoryDisabled,o as RadioButtonStoryError,h as __namedExportsOrder,b as default};
