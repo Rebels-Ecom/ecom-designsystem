a6fe374af583b33918eff8a31a8b7b67823f9b8d
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-01-08 12:53:41 +0000

    deploy: be8cba7a22b3b0af50eb8601b8514b446d2e5a6e

---
+++ assets/dynamic-filter.stories-dfd83838.js
@@ -0,0 +1,251 @@
+import{r as V,a as s,j as l,F as k}from"./jsx-runtime-a3a6c0b8.js";import{D as oe}from"./drawer-sidebar-b0bfef9e.js";import{c as z}from"./index-a587463d.js";import{A as ue}from"./index-4058f88b.js";import{m as $}from"./motion-54668070.js";import{I as M}from"./icon-5fefd509.js";import{B as G}from"./button-3cce3bbf.js";import{B as de,A as ve}from"./below-e3c1f628.js";import{E as q}from"./expandable-wrapper-5e4804ab.js";import{S as fe}from"./slider-26585f7f.js";import{F as ae}from"./flex-container-145d4933.js";import{C as pe}from"./checkbox-45f9076c.js";import{R as ge}from"./radio-button-f4956d18.js";const he="_dynamicFilter_16tv0_1",ye="_filterItem_16tv0_4",Ve="_filterName_16tv0_17",_e="_filterItemIcon_16tv0_22",be="_expandableWrapper_16tv0_27",ke="_showMoreButton_16tv0_35",xe="_showMoreLabel_16tv0_47",Fe="_checkboxItem_16tv0_52",Ne="_checkbox_16tv0_52",Ie="_label_16tv0_67",Se="_radioItem_16tv0_73",Me="_radio_16tv0_73",we="_filtersWrapper_16tv0_98",Be="_selectedFilters_16tv0_109",De="_filterButton_16tv0_120",Le="_selectedFilter_16tv0_109",Oe="_active_16tv0_155",Ce="_clear_16tv0_159",r={dynamicFilter:he,filterItem:ye,filterName:Ve,filterItemIcon:_e,expandableWrapper:be,showMoreButton:ke,showMoreLabel:xe,checkboxItem:Fe,checkbox:Ne,label:Ie,radioItem:Se,radio:Me,filtersWrapper:we,selectedFilters:Be,filterButton:De,selectedFilter:Le,active:Oe,clear:Ce},J=m=>{if(!m||!Array.isArray(m))return;const f=m==null?void 0:m.map(p=>Number(p.name)),_=Math.max(...f);return{min:Math.min(...f),max:Math.ceil(_/10)*10}},T=({filters:m,title:f,preSelected:_,onUpdate:o,loading:p,hideSliderFields:g,hideFilters:N,maxOptionsToShow:x=6})=>{var R;const[F,I]=V.useState(!1),[D,le]=V.useState([]),[u,A]=V.useState(_??[]),[h,j]=V.useState(!1),[W,re]=V.useState(!1),te=e=>{le(t=>t.includes(e)?t.filter(a=>a!==e):[...t,e])},ie=(e,t,a)=>{E({name:t,value:`${t}_${a.min}-${a.max}`},e,!0)},E=(e,t,a)=>{A(d=>{var S;const n=d;let c=[];const i=n==null?void 0:n.find(y=>y.name===t);if(!i)c=[...n,{name:t,selectedOptions:[e]}];else{const y=(S=i.selectedOptions)==null?void 0:S.find(b=>b.name===e.name);c=[...n==null?void 0:n.map(b=>b.name===i.name?a?{...i,selectedOptions:[e]}:y?{...i,selectedOptions:i.selectedOptions.filter(O=>O.name!==y.name)}:{...i,selectedOptions:[...i.selectedOptions,e]}:b)]}const v=c.filter(y=>!!y.selectedOptions.length);return o==null||o(e,v),v})},se=e=>{A(t=>{const a=t,n=(a==null?void 0:a.map(c=>({...c,selectedOptions:c.selectedOptions.filter(i=>i.name!==e.name)}))).filter(c=>!!c.selectedOptions.length);return o==null||o(e,n),n})},me=()=>{A([]),o==null||o(void 0,[])},ce=()=>I(!1);V.useEffect(()=>{F||j(!1)},[F]);const L=e=>{var t;return(t=e.options)==null?void 0:t.map((a,d)=>{var i;const n=u==null?void 0:u.find(v=>v.name===e.name),c=(i=n==null?void 0:n.selectedOptions)==null?void 0:i.find(v=>v.value===a.value);switch(e.type){case"radio":return s("button",{className:r.radioItem,onClick:()=>E(a,e.name,!0),disabled:p,children:[l(ge,{checked:!!c,id:a.value,name:a.value,className:r.radio,disabled:p}),l("span",{className:r.label,children:a.name})]},`${a.name}-${d}`);case"checkbox":default:return s("button",{className:r.checkboxItem,onClick:()=>E(a,e.name),disabled:p,children:[l(pe,{checked:!!c,id:a.value,name:a.value,className:r.checkbox,disabled:p}),l("span",{className:r.label,children:a.name})]},`${a.name}-${d}`)}})},P=V.useMemo(()=>s("div",{className:r.selectedFilters,children:[u==null?void 0:u.map(e=>{var t;return(t=e.selectedOptions)==null?void 0:t.map((a,d)=>{let n=a.name;return m!=null&&m.find(c=>{var i;return c.type==="range"&&((i=c.options)==null?void 0:i.find(v=>v.value===a.name))})&&(n=e.name),l(ue,{exitBeforeEnter:!0,presenceAffectsLayout:!0,initial:!1,children:s($.button,{className:z(r.selectedFilter,r.active),initial:{scale:.5,opacity:0},animate:{scale:1,opacity:1},exit:{scale:.25,opacity:0},onClick:()=>se(a),children:[l("span",{children:n}),l(M,{icon:"icon-x"})]},`${a.value}-${d}`)})})}),!!u.length&&l("button",{className:z(r.selectedFilter,r.clear),onClick:me,children:l(M,{icon:"icon-trash-2"})})]}),[u,m]),H=(R=u==null?void 0:u.flatMap(e=>e==null?void 0:e.selectedOptions))==null?void 0:R.length;return s(k,{children:[s("div",{className:r.filtersWrapper,children:[l(G,{type:"button",surface:"x",size:"xx-small",onClick:()=>I(!0),className:r.filterButton,children:"Filtrera"}),!N&&u.length>0&&s(k,{children:[l(de,{breakpoint:"md",children:e=>e&&s(k,{children:[l(G,{type:"button",surface:"x",size:"xx-small",onClick:()=>re(!W),className:r.filterButton,iconRight:{icon:W?"icon-chevron-up":"icon-chevron-down"},children:H>0?`Valda filter (${H})`:"Valda filter"}),l(q,{open:W,children:P})]})}),l(ve,{breakpoint:"md",children:e=>e&&l(k,{children:P})})]})]}),l(oe,{isOpen:F,onClose:ce,from:"left",width:"md",hideOverlay:!0,children:s("div",{className:r.dynamicFilter,children:[f&&l("h4",{className:r.title,children:f}),m==null?void 0:m.map((e,t)=>{var v,S,y,b,O;const a=D.includes(e.name),d=e.type==="range"?_==null?void 0:_.find(C=>C.name===e.name):void 0,n=(y=(S=(v=d==null?void 0:d.selectedOptions)==null?void 0:v[0])==null?void 0:S.value)==null?void 0:y.split("-"),c=n!=null&&n[0]?Number(n==null?void 0:n[0]):void 0,i=n!=null&&n[1]?Number(n==null?void 0:n[1]):void 0;return s("div",{className:r.filterCategory,children:[s("button",{className:r.filterItem,onClick:()=>te(e.name),children:[l("span",{className:r.filterName,children:e.name}),l(M,{className:r.filterItemIcon,icon:a?"icon-chevron-up":"icon-chevron-down"})]}),s(q,{open:a,className:r.expandableWrapper,children:[e.type==="range"?l(fe,{min:((b=J(e.options))==null?void 0:b.min)??0,max:((O=J(e.options))==null?void 0:O.max)??10,defaultMinVal:c,defaultMaxVal:i,withFields:!g,formatLabel:"kr",step:10,onChange:C=>ie(e.name,e.id,typeof C=="object"?C:{min:0,max:0}),disabled:p}):l(k,{children:L(e).slice(0,x)}),L(e).length>x&&e.type!=="range"?s(k,{children:[l(q,{open:h,children:L(e).slice(x,L(e).length)}),s(ae,{alignItems:"center",justifyContent:"center",children:[h&&s($.button,{initial:{scale:.5},animate:{scale:h?1:0},exit:{scale:.5},onClick:()=>j(!1),className:r.showMoreButton,children:[l("span",{className:r.showMoreLabel,children:h?"Se mindre":"Se mer"}),l(M,{icon:h?"icon-x-circle":"icon-plus-circle"})]},"open"),!h&&s($.button,{initial:{scale:.5},animate:{scale:h?0:1},exit:{scale:.5},onClick:()=>j(!0),className:r.showMoreButton,children:[l("span",{className:r.showMoreLabel,children:h?"Se mindre":"Se mer"}),l(M,{icon:h?"icon-x-circle":"icon-plus-circle"})]},"close")]})]}):null]})]},`${e.name}-${t}`)})]})})]})};try{T.displayName="DynamicFilter",T.__docgenInfo={description:"",displayName:"DynamicFilter",props:{filters:{defaultValue:null,description:"",name:"filters",required:!0,type:{name:"TFilterType[]"}},preSelected:{defaultValue:null,description:"",name:"preSelected",required:!0,type:{name:"TSelected[]"}},title:{defaultValue:null,description:"",name:"title",required:!1,type:{name:"string"}},onUpdate:{defaultValue:null,description:"",name:"onUpdate",required:!1,type:{name:"((option?: TOptionType, filters?: TSelected[]) => void) | undefined"}},loading:{defaultValue:null,description:"",name:"loading",required:!1,type:{name:"boolean"}},hideSliderFields:{defaultValue:null,description:"",name:"hideSliderFields",required:!1,type:{name:"boolean"}},hideFilters:{defaultValue:{value:"false"},description:"Defines if selected filters should be hidden",name:"hideFilters",required:!1,type:{name:"boolean"}},maxOptionsToShow:{defaultValue:{value:"6"},description:`Defines how many options to display
+The remaining will be hidden in a ExpandableWrapper (show more / show less)`,name:"maxOptionsToShow",required:!1,type:{name:"number"}}}}}catch{}const Te={title:"Design System/Molecules/Dynamic Filter",component:T},K=[{id:"1",country:"italien",price:"20",producer:"Birra Moretti"},{id:"2",country:"italien",price:"30",producer:"Birra Moretti"},{id:"3",country:"sverige",price:"40",producer:"Heineken"},{id:"4",country:"sverige",price:"10",producer:"Heineken"}],ne={render:m=>{const[f,_]=V.useState(),o=(g,N)=>{_(N)},p=V.useMemo(()=>K.filter(g=>f==null?void 0:f.find(N=>{var x;return(x=N.selectedOptions)==null?void 0:x.find(F=>{var I,D;return((I=F.name)==null?void 0:I.toLowerCase())===g.country||((D=F.name)==null?void 0:D.toLowerCase())===g.producer})})),[f]);return s(k,{children:[l(T,{onUpdate:o,title:"Filter",preSelected:m.preSelected??[],filters:m.filters}),l(ae,{flexDirection:"column",children:(p.length?p:K).map(g=>s("span",{style:{color:"blue"},children:[g.producer,", ",g.price,"kr"]},g.id))})]})}},w={...ne,args:{filters:[{name:"Listpris",type:"range",id:"rangefilter*V16062",options:[{name:"0",value:"rangefilter*V16062"},{name:"10",value:"rangefilter*V16062"},{name:"20",value:"rangefilter*V16062"},{name:"30",value:"rangefilter*V16062"},{name:"40",value:"rangefilter*V16062"},{name:"50",value:"rangefilter*V16062"},{name:"60",value:"rangefilter*V16062"},{name:"70",value:"rangefilter*V16062"},{name:"80",value:"rangefilter*V16062"},{name:"90",value:"rangefilter*V16062"},{name:"100",value:"rangefilter*V16062"},{name:"200",value:"rangefilter*V16062"},{name:"300",value:"rangefilter*V16062"},{name:"400",value:"rangefilter*V16062"},{name:"500",value:"rangefilter*V16062"},{name:"1000",value:"rangefilter*V16062"},{name:"1500",value:"rangefilter*V16062"},{name:"2000",value:"rangefilter*V16062"},{name:"2466",value:"rangefilter*V16062"}]},{name:"Land",type:"checkbox",options:[{name:"Sverige",value:"sverige"},{name:"Italien",value:"italien"},{name:"USA",value:"usa"},{name:"Belgien",value:"belgien"},{name:"Norge",value:"norge"},{name:"Irland",value:"irland"},{name:"Tyskland",value:"tyskland"},{name:"Nederländerna",value:"nederländerna"},{name:"Tjeckien",value:"tjeckien"}]},{name:"Producent",type:"checkbox",options:[{name:"Heineken",value:"heineken"},{name:"Birra Moretti",value:"birra-moretti"}]},{name:"Single Select",type:"radio",options:[{name:"Endast dryck",value:"drink-only"},{name:"Allt",value:"all"}]}]}},B={...ne,args:{filters:[{name:"Listpris",type:"range",id:"rangefilter*V16062",options:[{name:"0",value:"rangefilter*V16062"},{name:"10",value:"rangefilter*V16062"},{name:"20",value:"rangefilter*V16062"},{name:"30",value:"rangefilter*V16062"},{name:"40",value:"rangefilter*V16062"},{name:"50",value:"rangefilter*V16062"},{name:"60",value:"rangefilter*V16062"},{name:"70",value:"rangefilter*V16062"},{name:"80",value:"rangefilter*V16062"},{name:"90",value:"rangefilter*V16062"},{name:"100",value:"rangefilter*V16062"},{name:"200",value:"rangefilter*V16062"},{name:"300",value:"rangefilter*V16062"},{name:"400",value:"rangefilter*V16062"},{name:"500",value:"rangefilter*V16062"},{name:"1000",value:"rangefilter*V16062"},{name:"1500",value:"rangefilter*V16062"},{name:"2000",value:"rangefilter*V16062"},{name:"2466",value:"rangefilter*V16062"}]},{name:"Land",type:"checkbox",options:[{name:"Sverige",value:"sverige"},{name:"Italien",value:"italien"},{name:"USA",value:"usa"},{name:"Belgien",value:"belgien"},{name:"Norge",value:"norge"},{name:"Irland",value:"irland"},{name:"Tyskland",value:"tyskland"},{name:"Nederländerna",value:"nederländerna"},{name:"Tjeckien",value:"tjeckien"}]},{name:"Producent",type:"checkbox",options:[{name:"Heineken",value:"heineken"},{name:"Birra Moretti",value:"birra-moretti"}]},{name:"Single Select",type:"radio",options:[{name:"Endast dryck",value:"drink-only"},{name:"Allt",value:"all"}]}],preSelected:[{name:"Listpris",selectedOptions:[{name:"range-id",value:"20-600"}]},{name:"Land",selectedOptions:[{name:"Italien",value:"italien"}]}]}};var Q,X,Y;w.parameters={...w.parameters,docs:{...(Q=w.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  ...DynamicFilterStoryTemplate,
+  args: {
+    filters: ([{
+      name: 'Listpris',
+      type: 'range',
+      id: 'rangefilter*V16062',
+      options: [{
+        name: "0",
+        value: "rangefilter*V16062"
+      }, {
+        name: "10",
+        value: "rangefilter*V16062"
+      }, {
+        name: "20",
+        value: "rangefilter*V16062"
+      }, {
+        name: "30",
+        value: "rangefilter*V16062"
+      }, {
+        name: "40",
+        value: "rangefilter*V16062"
+      }, {
+        name: "50",
+        value: "rangefilter*V16062"
+      }, {
+        name: "60",
+        value: "rangefilter*V16062"
+      }, {
+        name: "70",
+        value: "rangefilter*V16062"
+      }, {
+        name: "80",
+        value: "rangefilter*V16062"
+      }, {
+        name: "90",
+        value: "rangefilter*V16062"
+      }, {
+        name: "100",
+        value: "rangefilter*V16062"
+      }, {
+        name: "200",
+        value: "rangefilter*V16062"
+      }, {
+        name: "300",
+        value: "rangefilter*V16062"
+      }, {
+        name: "400",
+        value: "rangefilter*V16062"
+      }, {
+        name: "500",
+        value: "rangefilter*V16062"
+      }, {
+        name: "1000",
+        value: "rangefilter*V16062"
+      }, {
+        name: "1500",
+        value: "rangefilter*V16062"
+      }, {
+        name: "2000",
+        value: "rangefilter*V16062"
+      }, {
+        name: "2466",
+        value: "rangefilter*V16062"
+      }]
+    }, {
+      name: 'Land',
+      type: 'checkbox',
+      options: [{
+        name: 'Sverige',
+        value: 'sverige'
+      }, {
+        name: 'Italien',
+        value: 'italien'
+      }, {
+        name: 'USA',
+        value: 'usa'
+      }, {
+        name: 'Belgien',
+        value: 'belgien'
+      }, {
+        name: 'Norge',
+        value: 'norge'
+      }, {
+        name: 'Irland',
+        value: 'irland'
+      }, {
+        name: 'Tyskland',
+        value: 'tyskland'
+      }, {
+        name: 'Nederländerna',
+        value: 'nederländerna'
+      }, {
+        name: 'Tjeckien',
+        value: 'tjeckien'
+      }]
+    }, {
+      name: 'Producent',
+      type: 'checkbox',
+      options: [{
+        name: 'Heineken',
+        value: 'heineken'
+      }, {
+        name: 'Birra Moretti',
+        value: 'birra-moretti'
+      }]
+    }, {
+      name: 'Single Select',
+      type: 'radio',
+      options: [{
+        name: 'Endast dryck',
+        value: 'drink-only'
+      }, {
+        name: 'Allt',
+        value: 'all'
+      }]
+    }] as TFilterType[])
+  }
+}`,...(Y=(X=w.parameters)==null?void 0:X.docs)==null?void 0:Y.source}}};var Z,U,ee;B.parameters={...B.parameters,docs:{...(Z=B.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  ...DynamicFilterStoryTemplate,
+  args: {
+    filters: ([{
+      name: 'Listpris',
+      type: 'range',
+      id: 'rangefilter*V16062',
+      options: [{
+        name: "0",
+        value: "rangefilter*V16062"
+      }, {
+        name: "10",
+        value: "rangefilter*V16062"
+      }, {
+        name: "20",
+        value: "rangefilter*V16062"
+      }, {
+        name: "30",
+        value: "rangefilter*V16062"
+      }, {
+        name: "40",
+        value: "rangefilter*V16062"
+      }, {
+        name: "50",
+        value: "rangefilter*V16062"
+      }, {
+        name: "60",
+        value: "rangefilter*V16062"
+      }, {
+        name: "70",
+        value: "rangefilter*V16062"
+      }, {
+        name: "80",
+        value: "rangefilter*V16062"
+      }, {
+        name: "90",
+        value: "rangefilter*V16062"
+      }, {
+        name: "100",
+        value: "rangefilter*V16062"
+      }, {
+        name: "200",
+        value: "rangefilter*V16062"
+      }, {
+        name: "300",
+        value: "rangefilter*V16062"
+      }, {
+        name: "400",
+        value: "rangefilter*V16062"
+      }, {
+        name: "500",
+        value: "rangefilter*V16062"
+      }, {
+        name: "1000",
+        value: "rangefilter*V16062"
+      }, {
+        name: "1500",
+        value: "rangefilter*V16062"
+      }, {
+        name: "2000",
+        value: "rangefilter*V16062"
+      }, {
+        name: "2466",
+        value: "rangefilter*V16062"
+      }]
+    }, {
+      name: 'Land',
+      type: 'checkbox',
+      options: [{
+        name: 'Sverige',
+        value: 'sverige'
+      }, {
+        name: 'Italien',
+        value: 'italien'
+      }, {
+        name: 'USA',
+        value: 'usa'
+      }, {
+        name: 'Belgien',
+        value: 'belgien'
+      }, {
+        name: 'Norge',
+        value: 'norge'
+      }, {
+        name: 'Irland',
+        value: 'irland'
+      }, {
+        name: 'Tyskland',
+        value: 'tyskland'
+      }, {
+        name: 'Nederländerna',
+        value: 'nederländerna'
+      }, {
+        name: 'Tjeckien',
+        value: 'tjeckien'
+      }]
+    }, {
+      name: 'Producent',
+      type: 'checkbox',
+      options: [{
+        name: 'Heineken',
+        value: 'heineken'
+      }, {
+        name: 'Birra Moretti',
+        value: 'birra-moretti'
+      }]
+    }, {
+      name: 'Single Select',
+      type: 'radio',
+      options: [{
+        name: 'Endast dryck',
+        value: 'drink-only'
+      }, {
+        name: 'Allt',
+        value: 'all'
+      }]
+    }] as TFilterType[]),
+    preSelected: [{
+      name: 'Listpris',
+      selectedOptions: [{
+        name: 'range-id',
+        value: '20-600'
+      }]
+    }, {
+      name: 'Land',
+      selectedOptions: [{
+        name: 'Italien',
+        value: 'italien'
+      }]
+    }]
+  }
+}`,...(ee=(U=B.parameters)==null?void 0:U.docs)==null?void 0:ee.source}}};const Ae=["Dynamic_Filter_Default","Dynamic_Filter_With_Pre_Selected"],Xe=Object.freeze(Object.defineProperty({__proto__:null,Dynamic_Filter_Default:w,Dynamic_Filter_With_Pre_Selected:B,__namedExportsOrder:Ae,default:Te},Symbol.toStringTag,{value:"Module"}));export{T as D,w as a,Xe as d};
---
+++ assets/table.stories-b68e0f82.js
@@ -0,0 +1,88 @@
+import{r as k,j as a,a as m,F as L}from"./jsx-runtime-a3a6c0b8.js";import{I as S}from"./icon-button-c1cc9278.js";import{c as h}from"./index-a587463d.js";import{B as P}from"./button-3cce3bbf.js";import{L as z}from"./loader-72deb8c4.js";import{H as A}from"./heading-808ea78c.js";import{C as F}from"./content-wrapper-c985da24.js";import"./_commonjsHelpers-de833af9.js";import"./ui-link-a18343fc.js";import"./icon-5fefd509.js";import"./motion-54668070.js";import"./button.module-de333a8d.js";const H="_table_1f266_1",K="_top_1f266_8",J="_heading_1f266_19",M="_mobile_1f266_27",Q="_columnTitle_1f266_36",U="_group_1f266_42",X="_icons_1f266_46",Y="_row_1f266_54",Z="_item_1f266_61",B="_desktop_1f266_66",ee="_column_1f266_36",ne="_equalWidthColumn_1f266_77",te="_lastTextColumn_1f266_82",oe="_icon_1f266_46",o={table:H,top:K,heading:J,mobile:M,columnTitle:Q,group:U,icons:X,row:Y,item:Z,desktop:B,column:ee,equalWidthColumn:ne,lastTextColumn:te,icon:oe},b=({listItems:s,hideColumnTitles:_=!1,equalWidthColumns:x=!1,listGap:G=0,loading:W,initialSortBy:w,title:C,action:N})=>{const[e,y]=k.useState(),I=s==null?void 0:s.sort((n,t)=>Object.keys(t).length-Object.keys(n).length)[0],l=s.length?Object.keys(I):[];k.useEffect(()=>{y({by:w??l[0],dir:"asc"})},[]);const j=(n,t)=>(e==null?void 0:e.dir)==="asc"?n[(e==null?void 0:e.by)??l[0]]>t[(e==null?void 0:e.by)??l[0]]?1:-1:(e==null?void 0:e.dir)==="desc"?n[(e==null?void 0:e.by)??l[0]]>t[(e==null?void 0:e.by)??l[0]]?-1:1:0,g=(n,t)=>a(S,{...n,size:"medium",noPadding:!0,isTransparent:!0,noBorder:!0},t),V=n=>s.sort((t,i)=>j(t,i)).map((t,i)=>typeof t[n]=="object"?a("div",{className:h(o.item,o.icon),children:g(t[n],i.toString())},`${t[n]}-${i}`):a("span",{className:o.item,children:t[n]},`${t[n]}-${i}`)),R={"--list-gap":`${G}rem`};return W?a(z,{visible:!0,size:"md"}):m("div",{className:o.table,style:R,children:[m("div",{className:o.top,children:[C&&a(A,{className:o.heading,order:5,children:C}),N&&a(P,{...N})]}),a("div",{className:o.mobile,children:s.map((n,t)=>{const i=Object.entries(n),r=i==null?void 0:i.filter(([c])=>!c.includes("icon")),u=i==null?void 0:i.filter(([c])=>c.includes("icon"));return m("div",{className:o.group,children:[r!=null&&r.length?r.map(([c,d])=>m("div",{className:o.row,children:[!_&&!c.includes("icon")?a("span",{className:o.columnTitle,children:`${c}: `}):"",a("span",{className:o.item,children:typeof d=="object"?g(d,`${c}-${t}`):d})]},`${c}-${t}`)):null,u!=null&&u.length?a("div",{className:o.icons,children:u.map(([c,d])=>typeof d=="object"&&g(d,`${c}-${t}`))}):null]},t)})}),a("div",{className:o.desktop,children:l==null?void 0:l.map((n,t)=>{const i=l==null?void 0:l.filter(r=>!r.includes("icon"));return m("div",{className:h(o.column,{[o.equalWidthColumn]:x,[o.textColumn]:!n.includes("icon"),[o.iconColumn]:n.includes("icon"),[o.lastTextColumn]:i[(i==null?void 0:i.length)-1]===n}),children:[!_&&a("span",{className:h(o.item,o.columnTitle,{[o.icon]:n.includes("icon")}),children:!n.includes("icon")&&m(L,{children:[n,a(S,{icon:(e==null?void 0:e.by)===n&&(e==null?void 0:e.dir)==="asc"?"icon-chevron-down":"icon-chevron-up",type:"button",onClick:()=>y({by:n,dir:(e==null?void 0:e.by)===n&&(e==null?void 0:e.dir)==="asc"?"desc":"asc"}),isTransparent:!0,noBorder:!0})]})}),V(n)]},`${n}-${t}`)})})]})};try{b.displayName="Table",b.__docgenInfo={description:"",displayName:"Table",props:{listItems:{defaultValue:null,description:"",name:"listItems",required:!0,type:{name:"TListItems"}},hideColumnTitles:{defaultValue:{value:"false"},description:"If true, no column titles will be display",name:"hideColumnTitles",required:!1,type:{name:"boolean"}},equalWidthColumns:{defaultValue:{value:"false"},description:"If true, each column will get equal width",name:"equalWidthColumns",required:!1,type:{name:"boolean"}},listGap:{defaultValue:{value:"0"},description:"Adds a gap (in rem) between the columns",name:"listGap",required:!1,type:{name:"number"}},initialSortBy:{defaultValue:{value:"'first column title'"},description:"What column title to sort by initially",name:"initialSortBy",required:!1,type:{name:"string"}},loading:{defaultValue:{value:"undefined"},description:"loading contacts indicator",name:"loading",required:!1,type:{name:"boolean"}},title:{defaultValue:{value:"undefined"},description:"Title to display on top of table",name:"title",required:!1,type:{name:"string"}},action:{defaultValue:null,description:"",name:"action",required:!1,type:{name:"IButton"}}}}}catch{}const ge={title:"Design System/Atoms/Table",component:b,parameters:{controls:{exclude:["listItems"]}}},$={render:s=>a(F,{children:a(b,{...s})})},p={...$,args:{listItems:[{name:"Name 1",status:"OK",id:"1",icon:{icon:"icon-alert-circle",onClick:()=>console.log("clicked"),type:"button"},icon2:{icon:"icon-alert-circle",onClick:()=>console.log("clicked"),type:"button"}},{name:"Name 6",status:"IN PROGRESS",id:"2",icon:{icon:"icon-alert-circle",onClick:()=>console.log("clicked"),type:"button"}},{name:"Name 3",status:"DONE",id:"3",icon:{icon:"icon-alert-circle",onClick:()=>console.log("clicked"),type:"button"}},{name:"Name 5",status:"DONE",id:"4",icon:{icon:"icon-alert-circle",onClick:()=>console.log("clicked"),type:"button"}},{name:"Name 2",status:"DONE",id:"5",icon:{icon:"icon-alert-circle",onClick:()=>console.log("clicked"),type:"button"}},{name:"Name 4",status:"DONE",id:"6",icon:{icon:"icon-alert-circle",onClick:()=>console.log("clicked"),type:"button"}}],hideColumnTitles:!1,equalWidthColumns:!1,listGap:0}},f={...$,args:{listItems:[{animal:"Dog",breed:"Golden Retriever",id:"1"},{animal:"Dog",breed:"German Shepherd",id:"2"},{animal:"Dog",breed:"English Setter",id:"3"}],hideColumnTitles:!1,equalWidthColumns:!1,listGap:0}};var q,T,v;p.parameters={...p.parameters,docs:{...(q=p.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  ...TableStoryTemplate,
+  args: ({
+    listItems: [{
+      name: 'Name 1',
+      status: 'OK',
+      id: '1',
+      "icon": {
+        icon: 'icon-alert-circle',
+        onClick: () => console.log('clicked'),
+        type: 'button'
+      },
+      "icon2": {
+        icon: 'icon-alert-circle',
+        onClick: () => console.log('clicked'),
+        type: 'button'
+      }
+    }, {
+      name: 'Name 6',
+      status: 'IN PROGRESS',
+      id: '2',
+      "icon": {
+        icon: 'icon-alert-circle',
+        onClick: () => console.log('clicked'),
+        type: 'button'
+      }
+    }, {
+      name: 'Name 3',
+      status: 'DONE',
+      id: '3',
+      "icon": {
+        icon: 'icon-alert-circle',
+        onClick: () => console.log('clicked'),
+        type: 'button'
+      }
+    }, {
+      name: 'Name 5',
+      status: 'DONE',
+      id: '4',
+      "icon": {
+        icon: 'icon-alert-circle',
+        onClick: () => console.log('clicked'),
+        type: 'button'
+      }
+    }, {
+      name: 'Name 2',
+      status: 'DONE',
+      id: '5',
+      "icon": {
+        icon: 'icon-alert-circle',
+        onClick: () => console.log('clicked'),
+        type: 'button'
+      }
+    }, {
+      name: 'Name 4',
+      status: 'DONE',
+      id: '6',
+      "icon": {
+        icon: 'icon-alert-circle',
+        onClick: () => console.log('clicked'),
+        type: 'button'
+      }
+    }],
+    hideColumnTitles: false,
+    equalWidthColumns: false,
+    listGap: 0
+  } as ITable)
+}`,...(v=(T=p.parameters)==null?void 0:T.docs)==null?void 0:v.source}}};var O,D,E;f.parameters={...f.parameters,docs:{...(O=f.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  ...TableStoryTemplate,
+  args: ({
+    listItems: [{
+      animal: 'Dog',
+      breed: 'Golden Retriever',
+      id: '1'
+    }, {
+      animal: 'Dog',
+      breed: 'German Shepherd',
+      id: '2'
+    }, {
+      animal: 'Dog',
+      breed: 'English Setter',
+      id: '3'
+    }],
+    hideColumnTitles: false,
+    equalWidthColumns: false,
+    listGap: 0
+  } as ITable)
+}`,...(E=(D=f.parameters)==null?void 0:D.docs)==null?void 0:E.source}}};const he=["TableStory","TableStoryTwo"];export{p as TableStory,f as TableStoryTwo,he as __namedExportsOrder,ge as default};
