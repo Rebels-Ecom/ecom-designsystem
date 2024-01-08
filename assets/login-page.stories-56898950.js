bd5e1e738bf4471e456082463f43e4ee2eb7528d
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-01-08 09:34:53 +0000

    deploy: 48b9e1e604750b5f6f407c8d93fc21b414dc5c58

---
+++ assets/login-page.stories-56898950.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-a3a6c0b8.js";import{L as S,a as l,b as _}from"./login-form.stories-a4a50341.js";import{A as P,a as A}from"./account-box-list.stories-e198e648.js";import"./_commonjsHelpers-de833af9.js";import"./logotype-564a64e9.js";import"./picture-7e21d28d.js";import"./index-a587463d.js";import"./form-91b1669a.js";import"./input-field-b4e65e28.js";import"./ui-link-a18343fc.js";import"./icon-5fefd509.js";import"./motion-54668070.js";import"./heading-808ea78c.js";import"./text-59fe9fb2.js";import"./button-8a1c536b.js";import"./button.module-d4e6cb42.js";import"./loader-72deb8c4.js";import"./flex-container-145d4933.js";import"./expandable-wrapper-5e4804ab.js";import"./Spendrups_logo_horizontal-038b757f.js";import"./account-box.stories-62f7685e.js";import"./button.stories-42790d50.js";import"./chunk-WFFRPTHA-a68c42c5.js";import"./preview-errors-dde4324f.js";import"./index-356e4a49.js";const h="_pageLayout_lb61a_1",x={pageLayout:h},t=({children:o})=>r("div",{className:x.pageLayout,children:o});try{t.displayName="LoginPage",t.__docgenInfo={description:"",displayName:"LoginPage",props:{}}}catch{}const W={title:"Design System/Templates/LoginPage",component:t},u={render:({...o})=>r(t,{children:r(_,{...o})})},T={render:({...o})=>r(t,{children:r(A,{...o})})},e={...u,args:{...S.args}},a={...u,args:{...l.args}},s={...T,args:{...P.args}};var n,i,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...LoginPageStoryTemplate,
+  args: {
+    ...LoginFormStory.args
+  }
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var g,p,c;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  ...LoginPageStoryTemplate,
+  args: {
+    ...LoginFormStory_Loading.args
+  }
+}`,...(c=(p=a.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var L,d,y;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  ...ChooseAccountStoryTemplate,
+  args: {
+    ...AccountBoxListStory.args
+  }
+}`,...(y=(d=s.parameters)==null?void 0:d.docs)==null?void 0:y.source}}};const X=["LoginPageStory","LoginPageStory_Loading","LoginPageStory_ChooseAccount"];export{e as LoginPageStory,s as LoginPageStory_ChooseAccount,a as LoginPageStory_Loading,X as __namedExportsOrder,W as default};
---
+++ assets/product-variant.stories-c6576cef.js
@@ -0,0 +1,12 @@
+import{r as d,j as m}from"./jsx-runtime-a3a6c0b8.js";import{P as o}from"./product-variant-3f6157eb.js";import{g as u}from"./picture-helper-1d1954b7.js";import"./_commonjsHelpers-de833af9.js";import"./radio-button-f4956d18.js";import"./index-a587463d.js";import"./picture-7e21d28d.js";import"./defaultFallbackImage-b43581a6.js";const N={title:"Design System/Molecules/Product/ ProductVariant",component:o},p={render:({...n})=>{const[s,c]=d.useState(!1);function i(){c(!0)}return m(o,{...n,onChange:i,checked:s})}},l="https://spendrups.cdn.storm.io/4237ad51-a9f9-45e5-a85a-72389f6e65bb",r={...p,args:{productName:"Norrlands Guld Export 5,3",variantName:"50cl Returglas",variantId:"1105101",country:"Sverige",priceStr:"22,68",salesUnit:"KLI",image:u("1105101",l)}};var t,a,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  ...ProductVariantStoryTemplate,
+  args: {
+    productName: 'Norrlands Guld Export 5,3',
+    variantName: '50cl Returglas',
+    variantId: '1105101',
+    country: 'Sverige',
+    priceStr: '22,68',
+    salesUnit: 'KLI',
+    image: getProductPicture('1105101', variantImage ? variantImage : fallbackProductImageUrl)
+  }
+}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const V=["ProductVariantStory"];export{r as ProductVariantStory,V as __namedExportsOrder,N as default};
