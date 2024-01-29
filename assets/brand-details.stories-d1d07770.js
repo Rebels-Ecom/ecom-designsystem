8e6829959aabe634409bd3692a190c7fc25bd0c0
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-01-29 11:22:22 +0000

    deploy: c3356ad94eed9679c95690a80f31051f026fe866

---
+++ assets/brand-details.stories-d1d07770.js
@@ -0,0 +1,15 @@
+import{a as c,j as e}from"./jsx-runtime-a3a6c0b8.js";import{F as p}from"./flex-container-145d4933.js";import{P as g}from"./picture-7e21d28d.js";import{T as x}from"./tags-list-3f7fda62.js";import{H as f}from"./heading-49a03c05.js";import{M as l}from"./max-width-7a20e242.js";import{T as h}from"./text-59fe9fb2.js";import{U as y}from"./ui-link-a18343fc.js";import{p as _}from"./pang-f66faff3.js";const s=({title:i,image:t,tags:n,link:o,...a})=>c(p,{flexDirection:"column",alignItems:"center",children:[e(g,{width:300,id:"brand-image",src:t.url,alt:t.alt,sources:[{srcset:`${t.mobileUrl??t.url}`,media:"(max-width: 767px)"},{srcset:`${t.url}`,media:"(min-width: 768px)"}]}),(n==null?void 0:n.length)&&e(x,{tagsList:n}),e(f,{order:3,margin:[0,0,.5],children:i}),a.richText&&e(l,{contentMaxWidth:"text",children:a.richText}),a.text&&e(l,{contentMaxWidth:"text",children:e(h,{align:"center",children:a.text})}),o&&e(y,{onSurface:"transparent",linkComponent:"a",isExternal:!0,href:o.url,children:o.text})]});try{s.displayName="BrandDetails",s.__docgenInfo={description:"",displayName:"BrandDetails",props:{title:{defaultValue:null,description:"",name:"title",required:!0,type:{name:"string"}},image:{defaultValue:null,description:"",name:"image",required:!0,type:{name:"{ url: string; mobileUrl?: string | undefined; alt?: string | undefined; }"}},tags:{defaultValue:null,description:"",name:"tags",required:!1,type:{name:"ITag[]"}},link:{defaultValue:null,description:"",name:"link",required:!1,type:{name:"{ url: string; text: string; }"}},richText:{defaultValue:null,description:"",name:"richText",required:!1,type:{name:"FC<RichTextProps>"}},text:{defaultValue:null,description:"",name:"text",required:!1,type:{name:"string"}}}}}catch{}const b={title:"Design System/Organisms/Brand Details",component:s},T={render:({...i})=>e(s,{...i})},r={...T,args:{title:"Pang Pang",image:{url:_,alt:"Pang Pang"},text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",link:{text:"Läs mer",url:"#"}}};var d,m,u;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  ...BrandDetailsStoryTemplate,
+  args: ({
+    title: 'Pang Pang',
+    image: {
+      url: pang,
+      alt: 'Pang Pang'
+    },
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    link: {
+      text: 'Läs mer',
+      url: '#'
+    }
+  } as TBrandDetails)
+}`,...(u=(m=r.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const D=["BrandDetailsStory"],I=Object.freeze(Object.defineProperty({__proto__:null,BrandDetailsStory:r,__namedExportsOrder:D,default:b},Symbol.toStringTag,{value:"Module"}));export{s as B,r as a,I as b};
---
+++ assets/campaign-message.stories-43c41cb2.js
@@ -0,0 +1,11 @@
+import{B as l}from"./button-369dade1.js";import{I as g}from"./icon-5fefd509.js";import{a as u,j as e}from"./jsx-runtime-a3a6c0b8.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";import"./button.module-16a60bae.js";import"./loader-72deb8c4.js";const d="_campaignMessage_409vb_1",_="_icon_409vb_13",y="_message_409vb_17",f="_button_409vb_32",s={campaignMessage:d,icon:_,message:y,button:f},t=({message:a,ctaText:m,icon:r,onClick:p})=>u("div",{className:s.campaignMessage,children:[r&&e(g,{...r,className:s.icon}),e("span",{className:s.message,children:a}),e(l,{className:s.button,surface:"primary",type:"button",onClick:p,children:e("span",{children:m})})]});try{t.displayName="CampaignMessage",t.__docgenInfo={description:"",displayName:"CampaignMessage",props:{message:{defaultValue:null,description:"",name:"message",required:!0,type:{name:"string"}},ctaText:{defaultValue:null,description:"",name:"ctaText",required:!0,type:{name:"string"}},icon:{defaultValue:null,description:"",name:"icon",required:!1,type:{name:"IIcon"}},onClick:{defaultValue:null,description:"",name:"onClick",required:!1,type:{name:"(() => void)"}}}}}catch{}const N={title:"Design System/Atoms/CampaignMessage",component:t},k={render:a=>e(t,{...a})},n={...k,args:{message:"Du har inte upplevt kraven för kampanjen",ctaText:"Lägg till produkter",onClick:()=>alert("clicked"),icon:{icon:"icon-alert-circle"}}};var o,i,c;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...CampaignMessageStoryTemplate,
+  args: {
+    message: 'Du har inte upplevt kraven för kampanjen',
+    ctaText: 'Lägg till produkter',
+    onClick: () => alert('clicked'),
+    icon: ({
+      icon: 'icon-alert-circle'
+    } as IIcon)
+  }
+}`,...(c=(i=n.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const S=["CampaignMessageStory"];export{n as CampaignMessageStory,S as __namedExportsOrder,N as default};
