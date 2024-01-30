c06546d289f17eaaf4a9d99a3626aa24cbc7ecc4
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-01-30 15:17:00 +0000

    deploy: 288f5db34adaa826cef12f674f9abd655deed622

--- assets/video.stories-f53502fc.js
+++
@@ -1,8 +0,0 @@
-import{V as d}from"./video-4611e7a5.js";import{j as o}from"./jsx-runtime-a3a6c0b8.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";const a=""+new URL("beerVideo-fb3d840b.mp4",import.meta.url).href,V={title:"Design System/Atoms/Video",component:d},n={render:({...s})=>o("div",{style:{margin:"1rem"},children:o(d,{...s})})},e={...n,args:{id:"videoOne",videoUrl:a,alt:"Video beer taps"}};var r,t,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  ...VideoStoryTemplate,
-  args: {
-    id: 'videoOne',
-    videoUrl: beerVideo,
-    alt: 'Video beer taps'
-  }
-}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const b=["VideoStory"];export{e as VideoStory,b as __namedExportsOrder,V as default};
