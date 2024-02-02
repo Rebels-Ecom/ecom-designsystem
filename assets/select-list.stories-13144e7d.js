5ba080662db18eee5f4ece6f63a5ba04909aa659
JelenaCumbo <JelenaCumbo@users.noreply.github.com>
2024-02-02 13:14:09 +0000

    deploy: 66045ced877d10568d0e66d257bd05e2374fbe6f

---
+++ assets/select-list.stories-13144e7d.js
@@ -0,0 +1,13 @@
+import{S as n}from"./select-list-a6eea82f.js";import{j as t,F as m}from"./jsx-runtime-a3a6c0b8.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";import"./useOnClickOutside-338168d6.js";import"./button-369dade1.js";import"./icon-5fefd509.js";import"./button.module-16a60bae.js";import"./loader-72deb8c4.js";import"./index-4058f88b.js";import"./motion-54668070.js";const F={title:"Design System/Atoms/Select List",component:n},i={render:s=>t(m,{children:t(n,{...s})})},e={...i,args:{placeholder:"Sortera",options:[{name:"Fallande",value:"fallande"},{name:"Stigande",value:"stigande"}]}};var r,a,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  ...SelectListStoryTemplate,
+  args: {
+    placeholder: 'Sortera',
+    options: [{
+      name: 'Fallande',
+      value: 'fallande'
+    }, {
+      name: 'Stigande',
+      value: 'stigande'
+    }]
+  }
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const h=["SelectListStory"];export{e as SelectListStory,h as __namedExportsOrder,F as default};
