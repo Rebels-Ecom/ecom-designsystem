c35ad59e0d273c3985310b5515a80a5a8bff3768
JelenaCumbo <jelena.cumbo@rebelandbird.com>
2024-02-13 10:04:48 +0100

    Bug fix - update campaign bug, update package version.

--- src/design-system/components/molecules/product-card/product-card.tsx
+++ src/design-system/components/molecules/product-card/product-card.tsx
@@ -124,17 +124,12 @@ function ProductCard({
       return
     }
 
-    setProduct((prevState) => {
-      const newProduct = {
-        ...prevState,
-        quantity: productQuantity.toString(),
-        totalPrice: convertNumToStr(myProduct.pricePerUnit * myProduct.itemNumberPerSalesUnit * productQuantity),
-      }
-
-      onChangeQuantity?.(newProduct)
-
-      return newProduct
-    })
+    const newProduct = {
+      ...myProduct,
+      quantity: productQuantity.toString(),
+      totalPrice: convertNumToStr(myProduct.pricePerUnit * myProduct.itemNumberPerSalesUnit * productQuantity),
+    }
+    onChangeQuantity?.(newProduct)
   }
 
   function handleVariantsButtonClick() {
