0187ac9ee78e6ce76d9fa5a75f5ac6c5531c9e39
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-02-27 15:34:31 +0000

    deploy: a598cbe4f0c024d32e85c29978eb7174391a1110

---
+++ assets/product-card.stories-61dba297.js
@@ -0,0 +1,64 @@
+import{r as B,j as i}from"./jsx-runtime-a3a6c0b8.js";import{g as V}from"./picture-helper-1d1954b7.js";import{c}from"./format-helper-6b4b1091.js";import{f as Ve}from"./defaultFallbackImage-b43581a6.js";import{P as he}from"./product-card-horizontal-9df5f10f.js";import{P as Ie}from"./product-card-vertical-7a4dbcb3.js";import{P as Se}from"./product-card-restricted-04da3be8.js";import{b as qe,a as Ue}from"./dummy-product-d82fa475.js";function u({cardDisplay:r,isRestrictedUser:e,product:y,loading:t,buttonLoading:h,disabled:I,addToCart:S,addToCartBtnLabel:q,hideCartButton:U,hidePrice:b,onChangeQuantity:D,productQuantityDisabled:O,defaultQuantity:d,onRemoveProduct:R,hideRemoveButton:me,limitedProductText:j,showFavoriteIcon:L,isFavoriteIconActive:z,onFavoriteIconClick:W,showAddToPurchaseListIcon:K,onSaveToPurchaseListClick:w,border:ge,displaySmallImage:fe,linkComponent:k,className:v,maxQuantity:P,sellerOnlyTooltipText:N,accessoryPotItemTooltipText:A,onClick:T}){if(!r)throw new Error("cardDisplay must be assigned");const{partNo:E,primaryImageUrl:ye,pricePerUnit:C,itemNumberPerSalesUnit:H,quantity:o,priceStr:M,activeCampaign:Q}=y,[$,_]=B.useState(!1),[n,F]=B.useState({...y,productImage:V(E,ye),quantity:o||"1",totalPrice:c(C*H*(d??o?parseInt(d??o):0)),selectedVariantId:E});B.useEffect(()=>{F(a=>({...a,quantity:o||"1",priceStr:M,pricePerUnit:C,totalPrice:c(C*H*(d??o?parseInt(d??o):0)),activeCampaign:Q}))},[o,M,C,Q]);function G(a){if(P&&a>P)return;const s={...n,quantity:a.toString(),totalPrice:c(n.pricePerUnit*n.itemNumberPerSalesUnit*a)};D?D(s):F(s)}function J(){_(!0)}function X(){_(!1)}function Pe(a){R&&R(a)}function Y(a){const s=n.partNo===a.variantId?parseInt(n.quantity):1;F(Ce=>({...Ce,partNo:a.variantId,productImage:a.image??Ve,packaging:a.variantName,price:a.price,priceStr:a.priceStr,pricePerUnit:a.pricePerUnit,pricePerUnitString:a.pricePerUnitString,salesUnit:a.salesUnit,itemNumberPerSalesUnit:a.itemNumberPerSalesUnit,totalPrice:c(a.price*a.itemNumberPerSalesUnit*s),quantity:s.toString(),selectedVariantId:a.variantId,sellerOnly:a.sellerOnly,activeCampaign:a.activeCampaign,productUrl:`/Product/${a.variantId}`})),_(!1)}return r==="horizontal"?i(he,{isRestrictedUser:e,cardDisplay:"horizontal",product:n,loading:t,buttonLoading:h,disabled:I,addToCart:S,addToCartBtnLabel:q,hideCartButton:U,hidePrice:b,onChangeQuantity:G,productQuantityDisabled:O,defaultQuantity:d,onClickRemoveProduct:Pe,hideRemoveButton:me,limitedProductText:j,showFavoriteIcon:L,isFavoriteIconActive:z,onFavoriteIconClick:W,showAddToPurchaseListIcon:K,onSaveToPurchaseListClick:w,border:ge,displaySmallImage:fe,linkComponent:k,className:v,maxQuantity:P,sellerOnlyTooltipText:N,accessoryPotItemTooltipText:A,onClick:T}):r==="vertical"?e?i(Se,{cardDisplay:"vertical",product:n,productImage:n.productImage,loading:t,buttonLoading:h,disabled:I,variantsOpen:$,onVariantsButtonClick:J,handlePackageChange:Y,selectedVariantId:n.selectedVariantId,addToCart:S,addToCartBtnLabel:q,hideCartButton:U,hidePrice:b,linkComponent:k,className:v,sellerOnlyTooltipText:N,accessoryPotItemTooltipText:A,onCloseVariants:X,onClick:T}):i(Ie,{cardDisplay:"vertical",product:n,productImage:n.productImage,loading:t,buttonLoading:h,disabled:I,variantsOpen:$,onVariantsButtonClick:J,handlePackageChange:Y,selectedVariantId:n.selectedVariantId,addToCart:S,addToCartBtnLabel:q,hideCartButton:U,hidePrice:b,onChangeQuantity:G,productQuantityDisabled:O,defaultQuantity:d,limitedProductText:j,showFavoriteIcon:L,isFavoriteIconActive:z,onFavoriteIconClick:W,showAddToPurchaseListIcon:K,onSaveToPurchaseListClick:w,linkComponent:k,className:v,maxQuantity:P,sellerOnlyTooltipText:N,accessoryPotItemTooltipText:A,onCloseVariants:X,onClick:T}):null}try{u.displayName="ProductCard",u.__docgenInfo={description:"",displayName:"ProductCard",props:{cardDisplay:{defaultValue:null,description:"",name:"cardDisplay",required:!0,type:{name:"enum",value:[{value:'"vertical"'},{value:'"horizontal"'}]}},isRestrictedUser:{defaultValue:null,description:"",name:"isRestrictedUser",required:!1,type:{name:"boolean"}},product:{defaultValue:null,description:"",name:"product",required:!0,type:{name:"IProduct"}},loading:{defaultValue:null,description:"",name:"loading",required:!0,type:{name:"boolean"}},buttonLoading:{defaultValue:null,description:"",name:"buttonLoading",required:!1,type:{name:"boolean"}},disabled:{defaultValue:null,description:"",name:"disabled",required:!1,type:{name:"boolean"}},hidePrice:{defaultValue:null,description:"",name:"hidePrice",required:!1,type:{name:"boolean"}},addToCart:{defaultValue:null,description:"",name:"addToCart",required:!0,type:{name:"CallableFunction"}},addToCartBtnLabel:{defaultValue:null,description:"",name:"addToCartBtnLabel",required:!0,type:{name:"string"}},hideCartButton:{defaultValue:null,description:"",name:"hideCartButton",required:!1,type:{name:"boolean"}},onChangeQuantity:{defaultValue:null,description:"",name:"onChangeQuantity",required:!1,type:{name:"CallableFunction"}},productQuantityDisabled:{defaultValue:null,description:"",name:"productQuantityDisabled",required:!1,type:{name:"boolean"}},defaultQuantity:{defaultValue:{value:"0"},description:"Sets a custom default quantity to start from",name:"defaultQuantity",required:!1,type:{name:"string"}},onRemoveProduct:{defaultValue:null,description:"",name:"onRemoveProduct",required:!1,type:{name:"CallableFunction"}},hideRemoveButton:{defaultValue:null,description:"",name:"hideRemoveButton",required:!1,type:{name:"boolean"}},limitedProductText:{defaultValue:null,description:"",name:"limitedProductText",required:!1,type:{name:"string"}},showFavoriteIcon:{defaultValue:null,description:"",name:"showFavoriteIcon",required:!1,type:{name:"boolean"}},isFavoriteIconActive:{defaultValue:null,description:"",name:"isFavoriteIconActive",required:!1,type:{name:"boolean"}},onFavoriteIconClick:{defaultValue:null,description:"",name:"onFavoriteIconClick",required:!1,type:{name:"CallableFunction"}},showAddToPurchaseListIcon:{defaultValue:null,description:"",name:"showAddToPurchaseListIcon",required:!1,type:{name:"boolean"}},onSaveToPurchaseListClick:{defaultValue:null,description:"",name:"onSaveToPurchaseListClick",required:!1,type:{name:"CallableFunction"}},border:{defaultValue:null,description:"",name:"border",required:!1,type:{name:"boolean"}},displaySmallImage:{defaultValue:null,description:"",name:"displaySmallImage",required:!1,type:{name:"boolean"}},linkComponent:{defaultValue:null,description:"",name:"linkComponent",required:!1,type:{name:"any"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},maxQuantity:{defaultValue:null,description:"",name:"maxQuantity",required:!1,type:{name:"number"}},sellerOnlyTooltipText:{defaultValue:null,description:"",name:"sellerOnlyTooltipText",required:!1,type:{name:"string"}},accessoryPotItemTooltipText:{defaultValue:null,description:"",name:"accessoryPotItemTooltipText",required:!1,type:{name:"string"}},onClick:{defaultValue:null,description:"",name:"onClick",required:!1,type:{name:"CallableFunction"}},productImage:{defaultValue:null,description:"",name:"productImage",required:!0,type:{name:"IPicture"}},variantsOpen:{defaultValue:null,description:"",name:"variantsOpen",required:!1,type:{name:"boolean"}},handlePackageChange:{defaultValue:null,description:"",name:"handlePackageChange",required:!0,type:{name:"CallableFunction"}},selectedVariantId:{defaultValue:null,description:"",name:"selectedVariantId",required:!1,type:{name:"string"}},onVariantsButtonClick:{defaultValue:null,description:"",name:"onVariantsButtonClick",required:!0,type:{name:"CallableFunction"}},onCloseVariants:{defaultValue:null,description:"",name:"onCloseVariants",required:!0,type:{name:"CallableFunction"}},onClickRemoveProduct:{defaultValue:null,description:"",name:"onClickRemoveProduct",required:!1,type:{name:"CallableFunction"}},removingProduct:{defaultValue:null,description:"",name:"removingProduct",required:!1,type:{name:"boolean"}}}}}catch{}function ce(r){return r.map(e=>({text:e.Text,shape:e.Shape?e.Shape:"pill",color:e.Class}))}function be(r,e){const y=e[0].VariantId;return e.map(t=>({productName:r,variantName:t.Name,variantId:t.VariantId,country:Array.isArray(t.ShortTexts)&&t.ShortTexts.length?t.ShortTexts[0]:"",priceStr:t.ListPricePerUnitString,price:t.ListPricePerUnit,salesUnit:t.SalesUnit,itemNumberPerSalesUnit:t.UnitsPerBaseUnit,imageUrl:t.PrimaryImageUrl,checked:t.VariantId===y,tags:ce(t.Tags),onChange:()=>{}}))}function pe(r){const e=r.Variants[0];return{partNo:e.VariantId,productUrl:r.ProductUrl,productName:r.DisplayName,primaryImageUrl:e.PrimaryImageUrl,country:Array.isArray(e.ShortTexts)&&e.ShortTexts.length?e.ShortTexts[0]:"",packaging:e.VariantFullName,priceStr:e.ListPricePerUnitString,price:e.ListPricePerUnit,salesUnit:e.SalesUnit,itemNumberPerSalesUnit:e.UnitsPerBaseUnit,tags:ce(e.Tags),quantity:"1",totalPrice:c(e.ListPricePerUnit*e.UnitsPerBaseUnit),productVariantList:be(r.DisplayName,r.Variants)}}const x=pe(qe),l=pe(Ue),ke={title:"Design System/Molecules/ProductCard",component:u,parameters:{controls:{exclude:["product","addToCart","onChangeQuantity","handlePackageChange","onVariantsButtonClick","productImage","linkComponent","onRemoveProduct","className","selectedVariantId","onClickRemoveProduct"]}}},p={render:r=>i(u,{...r}),args:{cardDisplay:"vertical",product:l,addToCart:()=>{},handlePackageChange:()=>{},loading:!1,onVariantsButtonClick:()=>{},productImage:V(l.partNo,l.primaryImageUrl),hideCartButton:!1,campaign:{title:"Kampanj",color:"#9A576F"}}},m={render:r=>i(u,{...r}),args:{cardDisplay:"vertical",product:x,defaultQuantity:"4",addToCart:()=>{},handlePackageChange:()=>{},loading:!1,onVariantsButtonClick:()=>{},productImage:V(l.partNo,l.primaryImageUrl),campaign:{title:"Kampanj",color:"#9A576F"}}},g={render:r=>i(u,{...r}),args:{cardDisplay:"vertical",product:x,addToCart:()=>{},handlePackageChange:()=>{},loading:!0,onVariantsButtonClick:()=>{},productImage:V(l.partNo,l.primaryImageUrl)}},f={render:r=>i(u,{...r}),args:{cardDisplay:"horizontal",product:x,addToCart:()=>{},loading:!1,campaign:{title:"Kampanj",color:"#9A576F"}}};var Z,ee,ae;p.parameters={...p.parameters,docs:{...(Z=p.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  render: args => {
+    return <ProductCard {...args} />;
+  },
+  args: {
+    cardDisplay: 'vertical',
+    product: productWineArgs,
+    addToCart: () => {},
+    handlePackageChange: () => {},
+    loading: false,
+    onVariantsButtonClick: () => {},
+    productImage: getProductPicture(productWineArgs.partNo, productWineArgs.primaryImageUrl),
+    hideCartButton: false,
+    campaign: {
+      title: 'Kampanj',
+      color: '#9A576F'
+    }
+  }
+}`,...(ae=(ee=p.parameters)==null?void 0:ee.docs)==null?void 0:ae.source}}};var re,te,ne;m.parameters={...m.parameters,docs:{...(re=m.parameters)==null?void 0:re.docs,source:{originalSource:`{
+  render: args => {
+    return <ProductCard {...args} />;
+  },
+  args: {
+    cardDisplay: 'vertical',
+    product: productNoVariantsArgs,
+    defaultQuantity: '4',
+    addToCart: () => {},
+    handlePackageChange: () => {},
+    loading: false,
+    onVariantsButtonClick: () => {},
+    productImage: getProductPicture(productWineArgs.partNo, productWineArgs.primaryImageUrl),
+    campaign: {
+      title: 'Kampanj',
+      color: '#9A576F'
+    }
+  }
+}`,...(ne=(te=m.parameters)==null?void 0:te.docs)==null?void 0:ne.source}}};var oe,ie,le;g.parameters={...g.parameters,docs:{...(oe=g.parameters)==null?void 0:oe.docs,source:{originalSource:`{
+  render: args => {
+    return <ProductCard {...args} />;
+  },
+  args: {
+    cardDisplay: 'vertical',
+    product: productNoVariantsArgs,
+    addToCart: () => {},
+    handlePackageChange: () => {},
+    loading: true,
+    onVariantsButtonClick: () => {},
+    productImage: getProductPicture(productWineArgs.partNo, productWineArgs.primaryImageUrl)
+  }
+}`,...(le=(ie=g.parameters)==null?void 0:ie.docs)==null?void 0:le.source}}};var ue,de,se;f.parameters={...f.parameters,docs:{...(ue=f.parameters)==null?void 0:ue.docs,source:{originalSource:`{
+  render: args => {
+    return <ProductCard {...args} />;
+  },
+  args: {
+    cardDisplay: 'horizontal',
+    product: productNoVariantsArgs,
+    addToCart: () => {},
+    loading: false,
+    campaign: {
+      title: 'Kampanj',
+      color: '#9A576F'
+    }
+  }
+}`,...(se=(de=f.parameters)==null?void 0:de.docs)==null?void 0:se.source}}};const ve=["ProductCardStory","ProductCardStory_NoVariants","ProductCardStory_Loading","ProductCardStory_Horizontal"],Oe=Object.freeze(Object.defineProperty({__proto__:null,ProductCardStory:p,ProductCardStory_Horizontal:f,ProductCardStory_Loading:g,ProductCardStory_NoVariants:m,__namedExportsOrder:ve,default:ke},Symbol.toStringTag,{value:"Module"}));export{u as P,f as a,p as b,Oe as p};
