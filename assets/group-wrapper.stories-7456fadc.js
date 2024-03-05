af304aaf53db8e1b174774788215054aac8cffb1
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-03-05 14:29:31 +0000

    deploy: 34ce070764028e01c6faaf86eebddbbaf96fb64e

--- assets/group-wrapper.stories-7456fadc.js
+++
@@ -1,9 +0,0 @@
-import{G as a}from"./group-wrapper-8f1ce8c7.js";import{H as i}from"./heading-c5df434e.js";import{j as e}from"./jsx-runtime-a3a6c0b8.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";const g={title:"Design System/Molecules/GroupWrapper",component:a},s={render:({...o})=>e("div",{style:{margin:"auto",maxWidth:"1800px"},children:e(a,{...o,children:o.children})})},r={...s,args:{children:e(i,{order:1,children:"Heading Component "}),position:"left",spacing:"md",noWrap:!0}};var p,n,t;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
-  ...GroupWrapperStoryTemplate,
-  args: {
-    children: <Heading order={1}>Heading Component </Heading>,
-    position: 'left',
-    spacing: 'md',
-    noWrap: true
-  }
-}`,...(t=(n=r.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};const W=["GroupWrapperStory"];export{r as GroupWrapperStory,W as __namedExportsOrder,g as default};
