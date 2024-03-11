73635c6386d95ca4effe07a3ee8d44f452adec74
JelenaCumbo <JelenaCumbo@users.noreply.github.com>
2024-03-11 13:19:40 +0000

    deploy: d23501c0f512b817fc49dd4f067d07bfe7b438d3

--- assets/carousel.stories-86280db5.js
+++
@@ -1,4 +0,0 @@
-import{C as t}from"./carousel-851c0365.js";import{j as e,F as m}from"./jsx-runtime-a3a6c0b8.js";import"./index-a587463d.js";import"./_commonjsHelpers-de833af9.js";const d={title:"Design System/Organisms/Carousel",component:t},n={render:p=>e(m,{children:e(t,{})})},r={...n,args:{}};var s,o,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  ...CarouselStoryTemplate,
-  args: {}
-}`,...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const g=["CarouselStory"];export{r as CarouselStory,g as __namedExportsOrder,d as default};
