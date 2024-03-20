2fc8e6fad1c2e864dc206e86c702b6a6b6d94a1e
Robert Pettersson <robert.pettersson@rebelandbird.com>
2024-03-20 11:05:39 +0100

    minor style updates

--- src/design-system/components/molecules/intro-block/intro-block.tsx
+++ src/design-system/components/molecules/intro-block/intro-block.tsx
@@ -3,6 +3,7 @@ import { Heading, Text } from "../../atoms";
 import { ContentWrapper, FlexContainer, MaxWidth } from "../../layouts"
 import styles from './intro-block.module.css'
 import { RichTextProps } from "../../../../types/other";
+import cx from 'classnames';
 
 
 
@@ -16,7 +17,7 @@ const IntroBlock = ({ title, ingress, richText }: TIntroBlock) => (
   <ContentWrapper padding={[2.5, 1]}>
     <MaxWidth contentMaxWidth='text'>
       <FlexContainer flexDirection='column' gap={1}>
-        {title && <h1 className={styles.title}>{title}</h1>}
+        {title && <h1 className={cx(styles.title, 'h2')}>{title}</h1>}
         {ingress && <p className={styles.ingress}>{ingress}</p>}
         {richText && richText}
       </FlexContainer>
