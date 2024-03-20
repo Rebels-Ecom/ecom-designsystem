2324231832850085949df458bad240532e84871a
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-03-20 10:17:31 +0000

    deploy: eccd25473cc433434af31afd79bd2c500f3e621a

---
+++ assets/intro-block.stories-6258a81a.js
@@ -0,0 +1,39 @@
+import{c}from"./index-a587463d.js";import{j as e,a as d}from"./jsx-runtime-a3a6c0b8.js";import{C as m}from"./content-wrapper-c985da24.js";import{M as g}from"./max-width-7a20e242.js";import{F as f}from"./flex-container-145d4933.js";import"./_commonjsHelpers-de833af9.js";const v="_title_dexw6_1",k="_ingress_dexw6_10",n={title:v,ingress:k},t=({title:a,ingress:s,richText:l})=>e(m,{padding:[2.5,1],children:e(g,{contentMaxWidth:"text",children:d(f,{flexDirection:"column",gap:1,children:[a&&e("h1",{className:c(n.title,"h2"),children:a}),s&&e("p",{className:n.ingress,children:s}),l&&l]})})});try{t.displayName="IntroBlock",t.__docgenInfo={description:"",displayName:"IntroBlock",props:{title:{defaultValue:null,description:"",name:"title",required:!1,type:{name:"string"}},ingress:{defaultValue:null,description:"",name:"ingress",required:!1,type:{name:"string"}},richText:{defaultValue:null,description:"",name:"richText",required:!1,type:{name:"FC<RichTextProps>"}}}}}catch{}const S={title:"Design System/Molecules/IntroBlock",component:t},h={render:({...a})=>e(t,{...a})},r={...h,args:{title:"En extraordnär rom till choklad",ingress:"En trevlig ölupplevelse är mer än bara stunden då drycken når smaklökarna. Det är många delar som ska falla på plats. Från det första intrycket som ett perfekt upphällt öl med vacker skumkrona ger, till smaken av ölets sista droppar. För att helhetsupplevelsen ska få toppbetyg behöver alla detaljer ses över. En av dessa detaljer är valet av glas.",richText:d("section",{className:"text-center",children:[e("div",{className:"OutlineElement Ltr BCX9 SCXW202138125",style:{color:"#000000",backgroundColor:"#ffffff",margin:0,padding:0},children:e("h5",{className:"Paragraph SCXW202138125 BCX9",style:{color:"windowtext",backgroundColor:"transparent",marginRight:0,marginLeft:0,padding:0,fontSize:"20px"},children:"Vi vill stolt presentera våra två nya profilglas, speciellt framtagna för att få varje serverad Mariestads att visa sig från sin allra bästa sida."})}),e("p",{className:"Paragraph SCXW202138125 BCX9",style:{color:"windowtext",backgroundColor:"transparent",marginRight:0,marginLeft:0,padding:0},children:"En trevlig ölupplevelse är mer än bara stunden då drycken når smaklökarna. Det är många delar som ska falla på plats. Från det första intrycket som ett perfekt upphällt öl med vacker skumkrona ger, till smaken av ölets sista droppar. För att helhetsupplevelsen ska få toppbetyg behöver alla detaljer ses över. En av dessa detaljer är valet av glas."}),e("p",{className:"Paragraph SCXW202138125 BCX9",style:{color:"windowtext",backgroundColor:"transparent",marginRight:0,marginLeft:0,padding:0},children:"I samarbete med en av världens främsta glasdesigner presenterar därför Mariestads stolt sina två nya profilglas. Dessa är speciellt framtagna och designade för att komplettera och förhöja upplevelsen av ett öl från Mariestads. "})]})}};var o,i,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...IntroBlockStoryTemplate,
+  args: {
+    title: 'En extraordnär rom till choklad',
+    ingress: 'En trevlig ölupplevelse är mer än bara stunden då drycken når smaklökarna. Det är många delar som ska falla på plats. Från det första intrycket som ett perfekt upphällt öl med vacker skumkrona ger, till smaken av ölets sista droppar. För att helhetsupplevelsen ska få toppbetyg behöver alla detaljer ses över. En av dessa detaljer är valet av glas.',
+    richText: <section className="text-center">
+            <div className="OutlineElement Ltr BCX9 SCXW202138125" style={{
+        color: '#000000',
+        backgroundColor: '#ffffff',
+        margin: 0,
+        padding: 0
+      }}>
+              <h5 className="Paragraph SCXW202138125 BCX9" style={{
+          color: 'windowtext',
+          backgroundColor: 'transparent',
+          marginRight: 0,
+          marginLeft: 0,
+          padding: 0,
+          fontSize: '20px'
+        }}>Vi vill stolt presentera&nbsp;våra två nya profilglas, speciellt framtagna för att&nbsp;få varje serverad&nbsp;Mariestads&nbsp;att visa sig från sin allra bästa sida.</h5>
+            </div>
+
+            <p className="Paragraph SCXW202138125 BCX9" style={{
+        color: 'windowtext',
+        backgroundColor: 'transparent',
+        marginRight: 0,
+        marginLeft: 0,
+        padding: 0
+      }}>En trevlig&nbsp;ölupplevelse&nbsp;är mer än bara stunden då drycken når smaklökarna. Det är många delar som ska falla på plats. Från det första intrycket som ett&nbsp;perfekt upphällt&nbsp;öl med vacker skumkrona ger, till smaken av ölets sista droppar. För att helhetsupplevelsen ska få toppbetyg behöver alla detaljer ses över. En av dessa&nbsp;detaljer&nbsp;är valet av glas.</p>
+            <p className="Paragraph SCXW202138125 BCX9" style={{
+        color: 'windowtext',
+        backgroundColor: 'transparent',
+        marginRight: 0,
+        marginLeft: 0,
+        padding: 0
+      }}>I samarbete med en av världens främsta&nbsp;glasdesigner presenterar därför Mariestads stolt sina två nya profilglas. Dessa är speciellt framtagna och designade för att komplettera och förhöja upplevelsen av&nbsp;ett öl från&nbsp;Mariestads.&nbsp;</p>
+          </section>
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const X=["IntroBlockStory"];export{r as IntroBlockStory,X as __namedExportsOrder,S as default};
