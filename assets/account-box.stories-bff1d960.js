3fb461567c96ed650b8603f600956ae559fac418
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-04-24 14:47:14 +0000

    deploy: 1d47e54be3b21900592097a4e9f3648e4196033f

---
+++ assets/account-box.stories-bff1d960.js
@@ -0,0 +1,24 @@
+import{c as S}from"./index-a587463d.js";import{a as d,j as e}from"./jsx-runtime-a3a6c0b8.js";import{B as T}from"./button-43643fd6.js";import{Button_Large_Icon_Right as f}from"./button.stories-037298f7.js";import{a as h}from"./chunk-WFFRPTHA-a68c42c5.js";const q="_accountBox_11l7a_1",V="_companyName_11l7a_17",L="_accountNumber_11l7a_18",j="_addressLabel_11l7a_22",F="_addressTitle_11l7a_27",I="_address_11l7a_22",n={accountBox:q,companyName:V,accountNumber:L,addressLabel:j,addressTitle:F,address:I};function s({companyName:a,accountNumber:b,contactPerson:i,addressLabel:m,addressTitle:u,address:l,zipcode:t,city:r,chooseAccountBtn:x,onClickChooseAccount:C}){const k=()=>{C()};return d("div",{className:n.accountBox,children:[d("div",{children:[e("p",{className:n.companyName,children:e("b",{children:a})}),e("p",{className:n.accountNumber,children:b}),i&&e("p",{children:i}),m&&e("p",{className:n.addressLabel,children:e("b",{children:m})}),u&&e("p",{className:n.addressTitle,children:u}),d("p",{className:S({[n.address]:!!u}),children:[l&&(t||r?`${l},`:l)," ",t&&(r?`${t},`:t)," ",r&&r]})]}),e(T,{...x,type:"button",surface:"primary",onClick:k})]})}try{s.displayName="AccountBox",s.__docgenInfo={description:"",displayName:"AccountBox",props:{companyName:{defaultValue:null,description:"",name:"companyName",required:!0,type:{name:"string"}},accountNumber:{defaultValue:null,description:"",name:"accountNumber",required:!0,type:{name:"string"}},contactPerson:{defaultValue:null,description:"",name:"contactPerson",required:!1,type:{name:"string"}},addressLabel:{defaultValue:null,description:"",name:"addressLabel",required:!1,type:{name:"string"}},addressTitle:{defaultValue:null,description:"",name:"addressTitle",required:!1,type:{name:"string"}},address:{defaultValue:null,description:"",name:"address",required:!1,type:{name:"string"}},zipcode:{defaultValue:null,description:"",name:"zipcode",required:!1,type:{name:"string"}},city:{defaultValue:null,description:"",name:"city",required:!1,type:{name:"string"}},chooseAccountBtn:{defaultValue:null,description:"",name:"chooseAccountBtn",required:!0,type:{name:"IButton"}},onClickChooseAccount:{defaultValue:null,description:"",name:"onClickChooseAccount",required:!0,type:{name:"CallableFunction"}}}}}catch{}const z={title:"Design System/Molecules/AccountBox",component:s},A={render:({...a})=>e(s,{...a})},o={...A,args:{companyName:"Beer Fest",accountNumber:"Kundnr: 186922",contactPerson:"Jon Jonsson",address:"Granängsringen 3",zipcode:"135 44",city:"Tyresö",chooseAccountBtn:f.args,onClickChooseAccount:h("clicked")}},c={...A,args:{companyName:"Beer Fest",accountNumber:"Kundnr: 186922",address:"Granängsringen 3",zipcode:"135 44",city:"Tyresö",chooseAccountBtn:f.args,onClickChooseAccount:h("clicked")}};var p,y,_;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...AccountBoxStoryTemplate,
+  args: {
+    companyName: 'Beer Fest',
+    accountNumber: 'Kundnr: 186922',
+    contactPerson: 'Jon Jonsson',
+    address: 'Granängsringen 3',
+    zipcode: '135 44',
+    city: 'Tyresö',
+    chooseAccountBtn: Button_Large_Icon_Right.args,
+    onClickChooseAccount: action('clicked')
+  }
+}`,...(_=(y=o.parameters)==null?void 0:y.docs)==null?void 0:_.source}}};var g,B,N;c.parameters={...c.parameters,docs:{...(g=c.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  ...AccountBoxStoryTemplate,
+  args: {
+    companyName: 'Beer Fest',
+    accountNumber: 'Kundnr: 186922',
+    address: 'Granängsringen 3',
+    zipcode: '135 44',
+    city: 'Tyresö',
+    chooseAccountBtn: Button_Large_Icon_Right.args,
+    onClickChooseAccount: action('clicked')
+  }
+}`,...(N=(B=c.parameters)==null?void 0:B.docs)==null?void 0:N.source}}};const G=["AccountBoxStory","AccountBoxStory_2"],R=Object.freeze(Object.defineProperty({__proto__:null,AccountBoxStory:o,AccountBoxStory_2:c,__namedExportsOrder:G,default:z},Symbol.toStringTag,{value:"Module"}));export{s as A,o as a,c as b,R as c};
---
+++ assets/icon-with-tooltip.stories-f6ba1f55.js
@@ -0,0 +1,6 @@
+import{I as s}from"./icon-with-tooltip-4ffb0da0.js";import{j as i}from"./jsx-runtime-a3a6c0b8.js";import"./extends-98964cd2.js";import"./index-79d56f34.js";import"./_commonjsHelpers-de833af9.js";import"./icon-5fefd509.js";import"./index-a587463d.js";const S={title:"Design System/Atoms/IconWithTooltip",component:s},n={render:p=>i(s,{icon:{icon:"icon-alert-circle"},content:"Icon with tooltip story"})},o={...n,args:{size:"sm"}};var t,r,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  ...TooltipStoryTemplate,
+  args: {
+    size: 'sm'
+  }
+}`,...(e=(r=o.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const g=["TooltipStory"];export{o as TooltipStory,g as __namedExportsOrder,S as default};
