d1e1194e56e35bd09672dcc86a3796a7ec81444e
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-05-08 09:10:50 +0000

    deploy: f9fb96042c73b9fee108c66e40d16d94b9ba82c5

---
+++ assets/login-page.stories-de90e7da.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-a3a6c0b8.js";import{L as S,a as _,b as l}from"./login-form.stories-611df8a3.js";import{A as P,a as A}from"./account-box-list.stories-9cfe64d9.js";import"./_commonjsHelpers-de833af9.js";import"./logotype-1eb74b8f.js";import"./picture-7e21d28d.js";import"./index-a587463d.js";import"./form-abda9ecf.js";import"./input-field-70802eab.js";import"./ui-link-9b0840f4.js";import"./icon-5fefd509.js";import"./form-helper-af1dc92c.js";import"./motion-54668070.js";import"./heading-47d2ab87.js";import"./text-32f6ed03.js";import"./button-43643fd6.js";import"./button.module-16a60bae.js";import"./loader-72deb8c4.js";import"./flex-container-145d4933.js";import"./expandable-wrapper-5e4804ab.js";import"./link-button-2ed2fc34.js";import"./Spendrups_logo_horizontal-038b757f.js";import"./account-box.stories-bff1d960.js";import"./button.stories-037298f7.js";import"./chunk-WFFRPTHA-a68c42c5.js";import"./preview-errors-dde4324f.js";import"./index-356e4a49.js";const h="_pageLayout_1n8t7_1",x={pageLayout:h},t=({children:o})=>r("div",{className:x.pageLayout,children:o});try{t.displayName="LoginPage",t.__docgenInfo={description:"",displayName:"LoginPage",props:{}}}catch{}const Y={title:"Design System/Templates/LoginPage",component:t},u={render:({...o})=>r(t,{children:r(l,{...o})})},T={render:({...o})=>r(t,{children:r(A,{...o})})},e={...u,args:{...S.args}},a={...u,args:{..._.args}},s={...T,args:{...P.args}};var n,i,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...LoginPageStoryTemplate,
+  args: {
+    ...LoginFormStory.args
+  }
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,g,c;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...LoginPageStoryTemplate,
+  args: {
+    ...LoginFormStory_Loading.args
+  }
+}`,...(c=(g=a.parameters)==null?void 0:g.docs)==null?void 0:c.source}}};var L,d,y;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  ...ChooseAccountStoryTemplate,
+  args: {
+    ...AccountBoxListStory.args
+  }
+}`,...(y=(d=s.parameters)==null?void 0:d.docs)==null?void 0:y.source}}};const Z=["LoginPageStory","LoginPageStory_Loading","LoginPageStory_ChooseAccount"];export{e as LoginPageStory,s as LoginPageStory_ChooseAccount,a as LoginPageStory_Loading,Z as __namedExportsOrder,Y as default};
