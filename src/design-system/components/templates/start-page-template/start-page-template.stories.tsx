3cc52f3be3dea2bd66b8f88ed687ca2570a10cba
Robert Pettersson <robert.pettersson@rebelandbird.com>
2024-06-25 10:52:58 +0200

    remove deprecated and unused components

--- src/design-system/components/templates/start-page-template/start-page-template.stories.tsx
+++ src/design-system/components/templates/start-page-template/start-page-template.stories.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import type { Meta } from '@storybook/react'
 import { HeroCarouselStory } from '../../organisms/hero-carousel/hero-carousel.stories'
-import { Three_Cards, Four_Cards, Full_Width } from '../../organisms/blog-card-list/blog-card-list.stories'
 import { OfferCardListStory } from '../../organisms/offer-card-list/offer-card-list.stories'
 import { Teaser_Round, Teaser_Square } from '../../molecules/teaser/teaser.stories'
 import { Standard_Header } from '../../organisms/header/header.stories'
 import { FooterStory } from '../../organisms/footer/footer.stories'
 import { FeaturedProductsCarouselStory } from '../../organisms/featured-products-carousel/featured-products-carousel.stories'
 import { CustomerTeaserStory } from '../../organisms/customer-teaser/customer-teaser.stories'
-import { BlogCardStoryFullWidth } from '../../molecules/blog-card/blog-card.stories'
-import { ContentWrapper, MaxWidth } from '../../layouts'
-import { BlogCardList, CustomerTeaser, FeaturedProductsCarousel, Footer, HeroCarousel, OfferCardList } from '../../organisms'
-import { BlogCard, Teaser } from '../../molecules'
+import { ContentWrapper } from '../../layouts'
+import { CustomerTeaser, FeaturedProductsCarousel, Footer, HeroCarousel, OfferCardList } from '../../organisms'
+import { Default } from '../../organisms/article-list/article-list.stories'
 
 const meta: Meta = {
   title: 'Design System/Templates/StartPageTemplate',
@@ -32,17 +30,9 @@ const StartPageStoryTemplate = {
       {args.header && args.header()}
       <HeroCarousel {...args.hero} />
       <ContentWrapper>
-        <BlogCardList {...args.blogList_Why_Spendrups} />
         <FeaturedProductsCarousel {...args.featuredProducts} />
         <OfferCardList {...args.offerCardsList} />
-        <BlogCardList {...args.blogList_Spendrups_Suppliers} />
-        <BlogCard {...args.blogFullwidth} />
         <CustomerTeaser {...args.customerTeaser} />
-        <Teaser {...args.teaserLeft1} />
-        <BlogCardList {...args.blogList_Spendrups_Brewing} />
-        <Teaser {...args.teaserRight} />
-        <Teaser {...args.teaserLeft2} />
-        <BlogCardList {...args.blogList_Spendrups_Equipment} />
         <FeaturedProductsCarousel {...args.featuredProducts} />
       </ContentWrapper>
       <Footer {...args.footer} />
@@ -55,17 +45,12 @@ export const StartPage_Guest_User = {
   args: {
     header: Standard_Header.render,
     hero: HeroCarouselStory.args,
-    blogList_Why_Spendrups: Three_Cards.args,
     featuredProducts: FeaturedProductsCarouselStory.args,
     offerCardsList: OfferCardListStory.args,
-    blogList_Spendrups_Suppliers: Four_Cards.args,
-    blogFullwidth: BlogCardStoryFullWidth.args,
     customerTeaser: CustomerTeaserStory.args,
     teaserRight: Teaser_Round.args,
-    blogList_Spendrups_Brewing: Full_Width.args,
     teaserLeft1: Teaser_Square.args,
     teaserLeft2: Teaser_Square.args,
-    blogList_Spendrups_Equipment: Four_Cards.args,
     footer:FooterStory.args
   }
 }
@@ -75,17 +60,12 @@ export const StartPage_Logged_In_User = {
   args: {
     header: Standard_Header.render,
     hero: HeroCarouselStory.args,
-    blogList_Why_Spendrups: Three_Cards.args,
     featuredProducts: FeaturedProductsCarouselStory.args,
     offerCardsList: OfferCardListStory.args,
-    blogList_Spendrups_Suppliers: Four_Cards.args,
-    blogFullwidth: BlogCardStoryFullWidth.args,
     customerTeaser: CustomerTeaserStory.args,
     teaserRight: Teaser_Round.args,
-    blogList_Spendrups_Brewing: Full_Width.args,
     teaserLeft1: Teaser_Square.args,
     teaserLeft2: Teaser_Square.args,
-    blogList_Spendrups_Equipment: Four_Cards.args,
     footer:FooterStory.args
   }
 }
@@ -95,17 +75,11 @@ export const StartPage_Admin_User = {
   args: {
     header: Standard_Header.render,
     hero: HeroCarouselStory.args,
-    blogList_Why_Spendrups: Four_Cards.args,
-    featuredProducts: Three_Cards.args,
     offerCardsList: OfferCardListStory.args,
-    blogList_Spendrups_Suppliers: Four_Cards.args,
-    blogFullwidth: BlogCardStoryFullWidth.args,
     customerTeaser: CustomerTeaserStory.args,
     teaserRight: Teaser_Round.args,
-    blogList_Spendrups_Brewing: Full_Width.args,
     teaserLeft1: Teaser_Square.args,
     teaserLeft2: Teaser_Square.args,
-    blogList_Spendrups_Equipment: Four_Cards.args,
     footer:FooterStory.args
   }
 }
