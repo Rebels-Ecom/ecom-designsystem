a84908a229a6f3f50694b8306d307f4647132a4a
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-08-06 10:52:44 +0000

    deploy: ab573328671b7790bdcd75ed89454f6ec1e5396c

---
+++ assets/form-group-input-text.stories-631abe79.js
@@ -0,0 +1,24 @@
+import{F as h}from"./form-group-44d7ee51.js";import{j as a,r as A}from"./jsx-runtime-03333307.js";import{InputTextStory as m}from"./input-text.stories-8f63f37e.js";import{I as R}from"./input-text-0d02e73b.js";import"./index-09749a66.js";import"./inline-helper-text-5f61e543.js";import"./inline-error-text-9fcd121f.js";import"./icon-cf6ebd85.js";import"./form-helper-af1dc92c.js";const w={title:"Design System/Molecules/FormGroup/InputText",component:h},s={render:(n,F)=>{const[I,b]=A.useState("");function k(D){b(D.currentTarget.value)}const v={onChange:k,value:I},E=!!n.errorText,j=F.story.includes("Disabled");return a.jsx("div",{style:{margin:"0 0 2rem 0",maxWidth:"616px"},children:a.jsx(h,{...n,formElementId:m.args.id,children:a.jsx(R,{...m.args,...v,disabled:j,isErroneous:E})})})}},p={label:"Vad letar du efter?",helperText:"Genom att skriva produkt namn, kan vi lättare hjälpa dig vidare.",requiredText:"(Obligatorisk)"},r={...s,args:{...p}},e={...s,args:{...p,errorText:"Du behöver fylla i produkt namn"}},o={...s,args:{...p}},t={...s,args:{...p,requiredText:"",labelRightText:a.jsx("a",{href:"/",children:"Se produkter"})}};var u,i,c;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  ...FormGroupStoryTemplate,
+  args: {
+    ...formGroupArgs
+  }
+}`,...(c=(i=r.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var l,d,x;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  ...FormGroupStoryTemplate,
+  args: {
+    ...formGroupArgs,
+    errorText: 'Du behöver fylla i produkt namn'
+  }
+}`,...(x=(d=e.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};var g,T,S;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  ...FormGroupStoryTemplate,
+  args: {
+    ...formGroupArgs
+  }
+}`,...(S=(T=o.parameters)==null?void 0:T.docs)==null?void 0:S.source}}};var G,f,y;t.parameters={...t.parameters,docs:{...(G=t.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  ...FormGroupStoryTemplate,
+  args: {
+    ...formGroupArgs,
+    requiredText: '',
+    labelRightText: <a href="/">Se produkter</a>
+  }
+}`,...(y=(f=t.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};const z=["FormGroupInputTextStory","FormGroupInputTextStoryError","FormGroupInputTextStoryDisabled","FormGroupInputTextStoryRightLabelText"];export{r as FormGroupInputTextStory,o as FormGroupInputTextStoryDisabled,e as FormGroupInputTextStoryError,t as FormGroupInputTextStoryRightLabelText,z as __namedExportsOrder,w as default};
---
+++ assets/order-confirmation.stories-63ba0120.js
@@ -0,0 +1,41 @@
+import{c as u}from"./index-09749a66.js";import{j as e}from"./jsx-runtime-03333307.js";import{c,d as p,b}from"./cart-product.stories-6097888d.js";import{C as f}from"./cart-product-list-f323bfda.js";import{H as v}from"./heading-ba18f9a9.js";import{T as s}from"./text-002528fe.js";import{M as y}from"./message-banner-8fd5f2bc.js";import{O as n}from"./order-confirmation-details-b1336e9b.js";import{G as g}from"./group-wrapper-5bc35e6d.js";import{B as o}from"./button-e2072245.js";const j="_orderConfirmation_1i816_1",x="_itemsList_1i816_16",D={orderConfirmation:j,itemsList:x};function i({children:r,className:a}){return e.jsx("div",{className:u(D.orderConfirmation,a||""),children:r})}i.__docgenInfo={description:"",methods:[],displayName:"OrderConfirmation",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},className:{required:!1,tsType:{name:"string"},description:""}}};const k={title:"Design System/Organisms/OrderConfirmation",component:i},C={render:({...r})=>e.jsx("div",{style:{margin:"auto",position:"relative"},children:e.jsxs(i,{...r,children:[e.jsx(v,{order:3,children:"Grattis"}),e.jsx(s,{children:"Din order är nu registrerad & kommer att förbereds för packning"}),e.jsxs(y,{type:"success",children:["Ditt order nr:  ",r.orderNumber]}),e.jsxs(s,{size:"small",children:[r.emailLabel,"   ",e.jsx("b",{children:r.email})]}),e.jsx(n,{detailItems:r.deliveryDetails,label:"Leveransuppgifter"}),e.jsx(n,{detailItems:r.pricingDetails}),e.jsx(n,{detailItems:r.paymentDetails,label:"Totalt"}),e.jsx(f,{children:r.itemsList.map(a=>e.jsx(b,{...a},Math.random()))}),e.jsxs(g,{spacing:"xl",children:[e.jsx(o,{children:"Fortsätt handla",type:"button",surface:"primary"}),e.jsx(o,{children:"Se över mina ordrar",type:"button",surface:"secondary"})]})]})})},L=[c.args,p.args],t={...C,args:{orderNumber:"12345678",emailLabel:"Vi har skickat din orderbekräftelse till ",email:"jon.jonsson@beerfest@com",deliveryDetailsLabel:"Leveransuppgifter",totalAmountLabel:"Totalt",deliveryDetails:[{label:"Postnummer",value:"135 44"},{label:"Adress",value:"Granängsringen 3"},{label:"Ort",value:"Tyresö"}],pricingDetails:[{label:"Summa produkter",value:"420,70"},{label:"Avdrag",value:"10%"},{label:"Frakt",value:"Gratis"}],paymentDetails:[{label:"Inklusive moms",value:"378,70 kr",isTotal:!0},{label:"Betalning",value:"Faktura"}],deliveryDateLabel:"Ditt aktuella leveransdatum",deliveryDate:"2022-06-03",itemsList:L}};var l,m,d;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  ...OrderConfirmationStoryTemplate,
+  args: {
+    orderNumber: '12345678',
+    emailLabel: 'Vi har skickat din orderbekräftelse till ',
+    email: 'jon.jonsson@beerfest@com',
+    deliveryDetailsLabel: 'Leveransuppgifter',
+    totalAmountLabel: 'Totalt',
+    deliveryDetails: [{
+      label: 'Postnummer',
+      value: '135 44'
+    }, {
+      label: 'Adress',
+      value: 'Granängsringen 3'
+    }, {
+      label: 'Ort',
+      value: 'Tyresö'
+    }],
+    pricingDetails: [{
+      label: 'Summa produkter',
+      value: '420,70'
+    }, {
+      label: 'Avdrag',
+      value: '10%'
+    }, {
+      label: 'Frakt',
+      value: 'Gratis'
+    }],
+    paymentDetails: [{
+      label: 'Inklusive moms',
+      value: '378,70 kr',
+      isTotal: true
+    }, {
+      label: 'Betalning',
+      value: 'Faktura'
+    }],
+    deliveryDateLabel: 'Ditt aktuella leveransdatum',
+    deliveryDate: '2022-06-03',
+    itemsList: cartProducts
+  }
+}`,...(d=(m=t.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const _=["OrderConfirmationStory"],F=Object.freeze(Object.defineProperty({__proto__:null,OrderConfirmationStory:t,__namedExportsOrder:_,default:k},Symbol.toStringTag,{value:"Module"}));export{t as O,i as a,F as o};
