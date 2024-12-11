a77665c7edfee46e0594e0d5df059bdd9b4e97bb
bobby-rebel <bobby-rebel@users.noreply.github.com>
2024-12-11 08:41:30 +0000

    deploy: 9d29830443fdc6348337e2c71d378d25a6f8bcdd

---
+++ assets/start-page-template.stories-5201e42f.js
@@ -0,0 +1,34 @@
+import{H as i,a as y}from"./hero-carousel.stories-e75ed449.js";import{O as m,a as T}from"./offer-card-list.stories-d55f3385.js";import{Teaser_Round as p,Teaser_Square as e}from"./teaser.stories-1de5e694.js";import{Standard_Header as n}from"./header.stories-0d4201a0.js";import{F as g,a as P}from"./footer.stories-d305bc3a.js";import{j as r}from"./jsx-runtime-03333307.js";import{C as R}from"./content-wrapper-adfa8526.js";import"./hero-59dbf3b5.js";import"./index-09749a66.js";import"./picture-41b0d9ed.js";import"./link-button-deeb7d4c.js";import"./ui-link-9ccf406a.js";import"./icon-cf6ebd85.js";import"./video-8ec94f12.js";import"./carousel-68e34069.js";import"./hero.stories-8c63e665.js";import"./picture.stories-b12bb409.js";import"./Content9-b09c4254.js";import"./pang-f66faff3.js";import"./defaultFallbackImage-b43581a6.js";import"./video.stories-7a2e024b.js";import"./offer-card.stories-ce538b6b.js";import"./tag-84e76236.js";import"./tag.stories-15375a30.js";import"./hooks-0265167d.js";import"./react-responsive-c4f2314e.js";import"./flex-container-7aff6506.js";import"./loader-5c06d14e.js";import"./desktop-navigation.stories-fb720231.js";import"./useOnClickOutside-9d2c35bb.js";import"./motion-9380e2c6.js";import"./tslib.es6-4d885d63.js";import"./icon-button-dbb2b0a4.js";import"./Spendrups_logo_horizontal-038b757f.js";import"./Spendrups_logo_vertical-4f973801.js";import"./cart-sidebar-7b20221c.js";import"./ui-date-picker.stories-dd1bed7d.js";import"./isNativeReflectConstruct-fe36982f.js";import"./index-821951ec.js";import"./button-adba1f05.js";import"./button.module-0b94080e.js";import"./admin-search.stories-ff61e006.js";import"./input-text-0d02e73b.js";import"./form-helper-af1dc92c.js";import"./useCloseOnEscape-f124367e.js";import"./mobile-navigation.stories-d84ce225.js";import"./index-61679f6b.js";import"./user-info-summary.stories-8ed048f5.js";import"./dropdown-list.stories-1d9f32e6.js";import"./top-nav-bar-d11e6695.js";import"./below-c0fccb92.js";import"./delivery-info-bar-21232035.js";import"./logotype-28a690f0.js";import"./drawer-sidebar-bc978e49.js";import"./group-wrapper-5bc35e6d.js";import"./heading-ba18f9a9.js";import"./newsletter-d8c50ab0.js";import"./footer-top-bar.stories-f0e0a422.js";import"./social-media-link-871839d2.js";const Or={title:"Design System/Templates/StartPageTemplate",parameters:{controls:{exclude:/.*/g,hideNoControlsWarning:!0}}},d={render:({...t})=>r.jsxs(r.Fragment,{children:[t.header&&t.header(),r.jsx(y,{...t.hero}),r.jsx(R,{children:r.jsx(T,{...t.offerCardsList})}),r.jsx(P,{...t.footer})]})},a={...d,args:{header:n.render,hero:i.args,offerCardsList:m.args,teaserRight:p.args,teaserLeft1:e.args,teaserLeft2:e.args,footer:g.args}},o={...d,args:{header:n.render,hero:i.args,offerCardsList:m.args,teaserRight:p.args,teaserLeft1:e.args,teaserLeft2:e.args,footer:g.args}},s={...d,args:{header:n.render,hero:i.args,offerCardsList:m.args,teaserRight:p.args,teaserLeft1:e.args,teaserLeft2:e.args,footer:g.args}};var f,S,u;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  ...StartPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    hero: HeroCarouselStory.args,
+    offerCardsList: OfferCardListStory.args,
+    teaserRight: Teaser_Round.args,
+    teaserLeft1: Teaser_Square.args,
+    teaserLeft2: Teaser_Square.args,
+    footer: FooterStory.args
+  }
+}`,...(u=(S=a.parameters)==null?void 0:S.docs)==null?void 0:u.source}}};var _,c,L;o.parameters={...o.parameters,docs:{...(_=o.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  ...StartPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    hero: HeroCarouselStory.args,
+    offerCardsList: OfferCardListStory.args,
+    teaserRight: Teaser_Round.args,
+    teaserLeft1: Teaser_Square.args,
+    teaserLeft2: Teaser_Square.args,
+    footer: FooterStory.args
+  }
+}`,...(L=(c=o.parameters)==null?void 0:c.docs)==null?void 0:L.source}}};var h,l,C;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  ...StartPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    hero: HeroCarouselStory.args,
+    offerCardsList: OfferCardListStory.args,
+    teaserRight: Teaser_Round.args,
+    teaserLeft1: Teaser_Square.args,
+    teaserLeft2: Teaser_Square.args,
+    footer: FooterStory.args
+  }
+}`,...(C=(l=s.parameters)==null?void 0:l.docs)==null?void 0:C.source}}};const Ur=["StartPage_Guest_User","StartPage_Logged_In_User","StartPage_Admin_User"];export{s as StartPage_Admin_User,a as StartPage_Guest_User,o as StartPage_Logged_In_User,Ur as __namedExportsOrder,Or as default};
