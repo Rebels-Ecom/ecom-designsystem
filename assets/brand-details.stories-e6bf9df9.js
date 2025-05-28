52f2cb7fe6e4e2b7cb1df8d1f931dd4fc7fa0188
bobby-rebel <bobby-rebel@users.noreply.github.com>
2025-05-28 07:33:17 +0000

    deploy: fe5f0d2dccde567174d693b61a0b81664216eb87

---
+++ assets/brand-details.stories-e6bf9df9.js
@@ -0,0 +1,22 @@
+import{j as e}from"./jsx-runtime-03333307.js";import{F as c}from"./flex-container-7aff6506.js";import{P as p}from"./picture-ffae027b.js";import{T as g}from"./tags-list-30883e29.js";import{H as x}from"./heading-ba18f9a9.js";import{M as d}from"./max-width-8514a3b3.js";import{T as f}from"./text-fc9325ee.js";import{U as y}from"./ui-link-974ac70a.js";import{p as h}from"./pang-f66faff3.js";const o=({title:a,image:r,tags:n,link:t,...s})=>e.jsxs(c,{flexDirection:"column",alignItems:"center",children:[e.jsx(p,{width:300,id:"brand-image",src:`${r.url}?w=300`,alt:r.alt,sources:[{srcset:`${r.mobileUrl??r.url}?w=300`,media:"(max-width: 767px)"},{srcset:`${r.url}?w=300`,media:"(min-width: 768px)"}],fetchPriority:"high",loading:"eager"}),(n==null?void 0:n.length)&&e.jsx(g,{tagsList:n}),e.jsx(x,{order:3,margin:[0,0,.5],children:a}),s.richText&&e.jsx(d,{contentMaxWidth:"text",children:s.richText}),s.text&&e.jsx(d,{contentMaxWidth:"text",children:e.jsx(f,{align:"center",children:s.text})}),(t==null?void 0:t.url)&&(t==null?void 0:t.text)&&e.jsx(y,{onSurface:"transparent",linkComponent:"a",isExternal:!0,href:t.url,children:t.text})]});o.__docgenInfo={description:"",methods:[],displayName:"BrandDetails",props:{title:{required:!0,tsType:{name:"string"},description:""},image:{required:!0,tsType:{name:"signature",type:"object",raw:`{
+  url: string;
+  mobileUrl?: string;
+  alt?: string;
+}`,signature:{properties:[{key:"url",value:{name:"string",required:!0}},{key:"mobileUrl",value:{name:"string",required:!1}},{key:"alt",value:{name:"string",required:!1}}]}},description:""},tags:{required:!1,tsType:{name:"Array",elements:[{name:"ITag"}],raw:"ITag[]"},description:""},link:{required:!1,tsType:{name:"signature",type:"object",raw:`{
+  url: string;
+  text: string;
+}`,signature:{properties:[{key:"url",value:{name:"string",required:!0}},{key:"text",value:{name:"string",required:!0}}]}},description:""}}};const b={title:"Design System/Organisms/Brand Details",component:o},T={render:({...a})=>e.jsx(o,{...a})},i={...T,args:{title:"Pang Pang",image:{url:h,alt:"Pang Pang"},text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",link:{text:"Läs mer",url:"#"}}};var m,l,u;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  ...BrandDetailsStoryTemplate,
+  args: ({
+    title: 'Pang Pang',
+    image: {
+      url: pang,
+      alt: 'Pang Pang'
+    },
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    link: {
+      text: 'Läs mer',
+      url: '#'
+    }
+  } as TBrandDetails)
+}`,...(u=(l=i.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const j=["BrandDetailsStory"],I=Object.freeze(Object.defineProperty({__proto__:null,BrandDetailsStory:i,__namedExportsOrder:j,default:b},Symbol.toStringTag,{value:"Module"}));export{o as B,i as a,I as b};
---
+++ assets/checkbox-list-item.stories-915b186d.js
@@ -0,0 +1,17 @@
+import{j as e,r as L}from"./jsx-runtime-03333307.js";import{C as j}from"./checkbox-aacef711.js";import{T as x}from"./text-fc9325ee.js";import{H as h}from"./heading-ba18f9a9.js";const _="_checkboxListItem_5jvjy_1",y="_itemWrapper_5jvjy_11",i={checkboxListItem:_,itemWrapper:y};function a({children:s,id:n,name:o,value:g,checked:b,onChange:I}){return s?e.jsxs("div",{className:i.checkboxListItem,children:[e.jsx("div",{className:i.itemWrapper,children:s}),e.jsx(j,{id:n,name:o,value:g,checked:b,onChange:I})]}):null}a.__docgenInfo={description:"",methods:[],displayName:"CheckboxListItem",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},id:{required:!0,tsType:{name:"string"},description:""},name:{required:!0,tsType:{name:"string"},description:""},value:{required:!0,tsType:{name:"string"},description:""},checked:{required:!0,tsType:{name:"boolean"},description:""},onChange:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const C={title:"Design System/Molecules/CheckboxListItem",component:a},k={render:({...s})=>{const[n,o]=L.useState(!1);return e.jsx("div",{style:{margin:"3rem auto",maxWidth:"1800px"},children:e.jsx(a,{...s,onChange:()=>o(!n)})})}},v=e.jsxs(e.Fragment,{children:[e.jsx(h,{order:5,noMargin:!0,children:"Loka stilla naturell 12 pack 50cl pet"}),e.jsx(x,{children:"Art.nr: 1224345"})]}),f=e.jsxs(e.Fragment,{children:[e.jsx(h,{order:5,noMargin:!0,children:"Heineken 5% 50cl engångsglas"}),e.jsx(x,{children:"Art.nr: 115112"})]}),t={...k,args:{children:v,id:"1224345",name:"1224345",value:"1224345"}},r={...k,args:{children:f,id:"115112",name:"115112",value:"115112"}};var c,m,d;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  ...CheckboxListItemStoryTemplate,
+  args: {
+    children: checkboxItemLoka,
+    id: '1224345',
+    name: '1224345',
+    value: '1224345'
+  }
+}`,...(d=(m=t.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var p,l,u;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...CheckboxListItemStoryTemplate,
+  args: {
+    children: checkboxItemHeineken,
+    id: '115112',
+    name: '115112',
+    value: '115112'
+  }
+}`,...(u=(l=r.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const T=["CheckboxListItem_Loka","CheckboxListItem_Heineken"],R=Object.freeze(Object.defineProperty({__proto__:null,CheckboxListItem_Heineken:r,CheckboxListItem_Loka:t,__namedExportsOrder:T,default:C},Symbol.toStringTag,{value:"Module"}));export{r as C,t as a,a as b,R as c};
