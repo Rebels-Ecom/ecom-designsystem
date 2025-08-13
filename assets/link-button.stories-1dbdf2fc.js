4a6688cec414e6da5b7f36acfebc2444a77dac0d
bobby-rebel <bobby-rebel@users.noreply.github.com>
2025-08-13 10:30:11 +0000

    deploy: 628381b8cfb40c9de647ac795f1cf1065844db8b

---
+++ assets/link-button.stories-1dbdf2fc.js
@@ -0,0 +1,15 @@
+import{L as m}from"./link-button-d6c3129e.js";import{UILinkStory as p}from"./ui-link.stories-73a290b9.js";import{j as g}from"./jsx-runtime-03333307.js";import"./index-09749a66.js";import"./ui-link-974ac70a.js";import"./icon-a32914e5.js";const B={title:"Design System/Atoms/LinkButton",component:m},c={render:({...u})=>g.jsx(m,{...u})},r={...c,args:{...p.args,size:"large",surface:"primary"}},t={...c,args:{...p.args,size:"large",surface:"primary"}};var o,e,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...LinkButtonStoryTemplate,
+  args: {
+    ...UILinkStory.args,
+    size: 'large',
+    surface: 'primary'
+  }
+}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var s,n,i;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...LinkButtonStoryTemplate,
+  args: {
+    ...UILinkStory.args,
+    size: 'large',
+    surface: 'primary'
+  }
+}`,...(i=(n=t.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};const f=["LinkButtonStory","LinkButtonStory_GoToCart"];export{r as LinkButtonStory,t as LinkButtonStory_GoToCart,f as __namedExportsOrder,B as default};
