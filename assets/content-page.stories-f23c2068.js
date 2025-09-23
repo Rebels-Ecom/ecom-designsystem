c22b7ab631cfebe9e61c5db97dc414d0fd21a102
bobby-rebel <bobby-rebel@users.noreply.github.com>
2025-09-23 11:57:23 +0000

    deploy: 4b9ead706e916201288233dd3fa79304a2c0bb6b

---
+++ assets/content-page.stories-f23c2068.js
@@ -0,0 +1,17 @@
+import{Standard_Header as d}from"./header.stories-5d8e96ad.js";import{F as c,a as u}from"./footer.stories-c37d9898.js";import{B as g,a as x}from"./breadcrumbs.stories-f7fb442f.js";import{R as y,a as h,b as l}from"./rich-text.stories-515d4cb9.js";import{j as r}from"./jsx-runtime-03333307.js";import{C as b}from"./content-wrapper-adfa8526.js";import"./hooks-0265167d.js";import"./react-responsive-c4f2314e.js";import"./flex-container-7aff6506.js";import"./index-09749a66.js";import"./loader-5c06d14e.js";import"./desktop-navigation.stories-b7d33aea.js";import"./useOnClickOutside-9d2c35bb.js";import"./motion-9380e2c6.js";import"./tslib.es6-4d885d63.js";import"./icon-a32914e5.js";import"./icon-button-aa68e962.js";import"./ui-link-974ac70a.js";import"./Spendrups_logo_horizontal-038b757f.js";import"./Spendrups_logo_vertical-4f973801.js";import"./cart-sidebar-7b20221c.js";import"./ui-date-picker.stories-e49fa451.js";import"./defineProperty-dca1f18b.js";import"./isNativeReflectConstruct-fe36982f.js";import"./index-821951ec.js";import"./button-1de8d46b.js";import"./button.module-867d0823.js";import"./admin-search.stories-daf7a8cd.js";import"./input-text-e1680c6c.js";import"./form-helper-af1dc92c.js";import"./useCloseOnEscape-f124367e.js";import"./mobile-navigation.stories-12a81b0e.js";import"./index-61679f6b.js";import"./user-info-summary.stories-82e81bdb.js";import"./dropdown-list.stories-1d9f32e6.js";import"./top-nav-bar-1091afd2.js";import"./below-c0fccb92.js";import"./delivery-info-bar-c1251136.js";import"./logotype-91e51e50.js";import"./picture-6b702374.js";import"./drawer-sidebar-7bd0792d.js";import"./group-wrapper-5bc35e6d.js";import"./heading-5bc32a9e.js";import"./link-button-a77c9463.js";import"./newsletter-847c0f02.js";import"./footer-top-bar.stories-909a95e1.js";import"./social-media-link-3616019b.js";import"./picture.stories-3150d164.js";import"./Content9-b09c4254.js";import"./pang-f66faff3.js";import"./defaultFallbackImage-b43581a6.js";const hr={title:"Design System/Templates/ContentPage",parameters:{controls:{exclude:/.*/g,hideNoControlsWarning:!0}}},S={render:({...t})=>r.jsxs(r.Fragment,{children:[t.header&&t.header(),r.jsxs(b,{children:[r.jsx(x,{textWidth:"narrow",...t.breadcrumbs}),r.jsx(l,{...t.richText})]}),r.jsx(u,{...t.footer})]})},e={...S,args:{header:d.render,breadcrumbs:g.args,richText:y.args,footer:c.args}},o={...S,args:{header:d.render,breadcrumbs:g.args,richText:h.args,footer:c.args}};var a,m,i;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  ...ContentPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    breadcrumbs: BreadcrumbsStory_Sustainability.args,
+    richText: RichTextStory.args,
+    footer: FooterStory.args
+  }
+}`,...(i=(m=e.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var p,s,n;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...ContentPageStoryTemplate,
+  args: {
+    header: Standard_Header.render,
+    breadcrumbs: BreadcrumbsStory_Sustainability.args,
+    richText: RichTextStory_Beer.args,
+    footer: FooterStory.args
+  }
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const lr=["ContentPageTemplateStory","ContentPageTemplateStory_Beer"];export{e as ContentPageTemplateStory,o as ContentPageTemplateStory_Beer,lr as __namedExportsOrder,hr as default};
---
+++ assets/modal.stories-fde6b3c3.js
@@ -0,0 +1,19 @@
+import{j as r,r as L}from"./jsx-runtime-03333307.js";import{M as C}from"./modal-1f812909.js";import{C as s,a}from"./create-list-form.stories-248f0069.js";import"./index-09749a66.js";import"./generic-helper-ddbede2e.js";import"./debounce-27b61785.js";import"./isSymbol-9d21be03.js";import"./index-821951ec.js";import"./motion-9380e2c6.js";import"./tslib.es6-4d885d63.js";import"./icon-button-aa68e962.js";import"./icon-a32914e5.js";import"./ui-link-974ac70a.js";import"./index-61679f6b.js";import"./loading-overlay-89d0941c.js";import"./loader-5c06d14e.js";import"./logotype-91e51e50.js";import"./picture-6b702374.js";import"./form-group-4de333d7.js";import"./inline-helper-text-5f61e543.js";import"./inline-error-text-caf17fd4.js";import"./input-text-e1680c6c.js";import"./form-helper-af1dc92c.js";import"./textarea-383fe271.js";import"./button-1de8d46b.js";import"./button.module-867d0823.js";import"./Spendrups_logo_horizontal-038b757f.js";const V={title:"Design System/Molecules/Modal",component:C},m={render:({...M})=>{const[h,i]=L.useState(!1);return r.jsxs("div",{children:[r.jsx("button",{onClick:()=>i(!0),children:"Open Modal"}),r.jsx(C,{...M,open:h,onClose:()=>i(!1),children:r.jsx(s,{...a.args,onCancel:()=>i(!1)})})]})}},e={...m,args:{children:r.jsx(s,{...a.args})}},o={...m,args:{children:r.jsx(s,{...a.args}),dismissable:!0}},t={...m,args:{children:r.jsx(s,{...a.args}),dismissable:!0,backdropType:"light"}};var p,n,l;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...ModalStoryTemplate,
+  args: {
+    children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>
+  }
+}`,...(l=(n=e.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var d,c,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  ...ModalStoryTemplate,
+  args: {
+    children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>,
+    dismissable: true
+  }
+}`,...(g=(c=o.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};var u,S,y;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  ...ModalStoryTemplate,
+  args: {
+    children: <CreateListForm {...CreateListFormStory.args}></CreateListForm>,
+    dismissable: true,
+    backdropType: 'light'
+  }
+}`,...(y=(S=t.parameters)==null?void 0:S.docs)==null?void 0:y.source}}};const W=["ModalStory","ModalStory_Dismissable","ModalStory_LightBackdrop"];export{e as ModalStory,o as ModalStory_Dismissable,t as ModalStory_LightBackdrop,W as __namedExportsOrder,V as default};
